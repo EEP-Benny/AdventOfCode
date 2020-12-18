a2d7f374d832c8a98a22ae7357df1811d29b2d99
Benjamin Hogl <9270901+EEP-Benny@users.noreply.github.com>
2020-12-18 09:25:23 +0100

    Day 18, part 2, ~50min

--- 2020/18.test.ts
+++ 2020/18.test.ts
@@ -1,34 +1,63 @@
-import { evaluateExpression, parseExpression } from "./18";
+import {
+  evaluateExpression,
+  parseExpressionArray,
+  parseExpressionWithInvertedPrecedence,
+  parseExpressionWithoutPrecedence,
+} from "./18";
 
-test("parseExpression", () => {
-  expect(parseExpression("1 + 2 * 3")).toEqual({
+test("parseExpressionArray", () => {
+  expect(parseExpressionArray("1 + 2 * 3")).toEqual([1, "+", 2, "*", 3]);
+  expect(parseExpressionArray("1 + (2 * 3)")).toEqual([1, "+", [2, "*", 3]]);
+});
+
+test("parseExpressionWithoutPrecedence", () => {
+  expect(parseExpressionWithoutPrecedence("1 + 2 * 3")).toEqual({
     operator: "*",
     lhs: { operator: "+", lhs: 1, rhs: 2 },
     rhs: 3,
   });
-  expect(parseExpression("1 + (2 * 3)")).toEqual({
+  expect(parseExpressionWithoutPrecedence("1 + (2 * 3)")).toEqual({
     operator: "+",
     lhs: 1,
     rhs: { operator: "*", lhs: 2, rhs: 3 },
   });
-  expect(parseExpression("2 * 3 + (4 * 5)")).toEqual({
+  expect(parseExpressionWithoutPrecedence("2 * 3 + (4 * 5)")).toEqual({
     operator: "+",
     lhs: { operator: "*", lhs: 2, rhs: 3 },
     rhs: { operator: "*", lhs: 4, rhs: 5 },
   });
 });
 
-test("evaluateExpression", () => {
-  const parseAndEvaluate = (str: string) =>
-    evaluateExpression(parseExpression(str));
-  expect(parseAndEvaluate("1 + 2 * 3 + 4 * 5 + 6")).toBe(71);
-  expect(parseAndEvaluate("1 + (2 * 3) + (4 * (5 + 6))")).toBe(51);
-  expect(parseAndEvaluate("2 * 3 + (4 * 5)")).toBe(26);
-  expect(parseAndEvaluate("5 + (8 * 3 + 9 + 3 * 4 * 3)")).toBe(437);
-  expect(parseAndEvaluate("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))")).toBe(
-    12240
-  );
+test("parseExpressionWithInvertedPrecedence", () => {
   expect(
-    parseAndEvaluate("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2")
-  ).toBe(13632);
+    parseExpressionWithInvertedPrecedence("1 + 2 * 3 + 4 * 5 + 6")
+  ).toEqual({
+    operator: "*",
+    lhs: {
+      operator: "*",
+      lhs: { operator: "+", lhs: 1, rhs: 2 },
+      rhs: { operator: "+", lhs: 3, rhs: 4 },
+    },
+    rhs: { operator: "+", lhs: 5, rhs: 6 },
+  });
+});
+
+test("evaluateExpression", () => {
+  const pe1 = (str: string) =>
+    evaluateExpression(parseExpressionWithoutPrecedence(str));
+  expect(pe1("1 + 2 * 3 + 4 * 5 + 6")).toBe(71);
+  expect(pe1("1 + (2 * 3) + (4 * (5 + 6))")).toBe(51);
+  expect(pe1("2 * 3 + (4 * 5)")).toBe(26);
+  expect(pe1("5 + (8 * 3 + 9 + 3 * 4 * 3)")).toBe(437);
+  expect(pe1("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))")).toBe(12240);
+  expect(pe1("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2")).toBe(13632);
+
+  const pe2 = (str: string) =>
+    evaluateExpression(parseExpressionWithInvertedPrecedence(str));
+  expect(pe2("1 + 2 * 3 + 4 * 5 + 6")).toBe(231);
+  expect(pe2("1 + (2 * 3) + (4 * (5 + 6))")).toBe(51);
+  expect(pe2("2 * 3 + (4 * 5)")).toBe(46);
+  expect(pe2("5 + (8 * 3 + 9 + 3 * 4 * 3)")).toBe(1445);
+  expect(pe2("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))")).toBe(669060);
+  expect(pe2("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2")).toBe(23340);
 });
--- 2020/18.ts
+++ 2020/18.ts
@@ -1,5 +1,6 @@
 import { getInputArray } from "../utils";
 
+export type ExpressionArray = ("+" | "*" | ExpressionArray)[] | number;
 export type Expression =
   | number
   | {
@@ -7,35 +8,96 @@ export type Expression =
       lhs: Expression;
       rhs: Expression;
     };
-export function parseExpression(str: string): Expression {
+
+export function parseExpressionArray(str: string): ExpressionArray {
+  str = str.trim();
   const strAsNumber = +str;
   if (!Number.isNaN(strAsNumber)) {
     return strAsNumber;
   }
+  const arr: ExpressionArray = [];
   let openParentheses = 0;
-  for (let i = str.length - 1; i >= 0; i--) {
+  let lastSplitAtIndex = 0;
+  for (let i = 0; i <= str.length; i++) {
     switch (str[i]) {
-      case ")":
+      case "(":
         openParentheses++;
         break;
-      case "(":
+      case ")":
         openParentheses--;
         break;
       case "+":
       case "*":
         if (openParentheses === 0) {
-          return {
-            operator: str[i] as "+" | "*",
-            lhs: parseExpression(str.substring(0, i - 1).trim()),
-            rhs: parseExpression(str.substring(i + 1).trim()),
-          };
+          arr.push(
+            parseExpressionArray(str.substring(lastSplitAtIndex, i - 1))
+          );
+          arr.push(str[i] as "+" | "*");
+          lastSplitAtIndex = i + 1;
         }
+        break;
     }
   }
-  if (str.startsWith("(") && str.endsWith(")")) {
-    return parseExpression(str.substring(1, str.length - 1));
+  if (lastSplitAtIndex === 0) {
+    if (str.startsWith("(") && str.endsWith(")")) {
+      return parseExpressionArray(str.substring(1, str.length - 1));
+    }
+    // we didn't make any progress
+    throw `Illegal expression: ${str}`;
   }
-  throw `Invalid expression: ${str}`;
+  arr.push(parseExpressionArray(str.substring(lastSplitAtIndex)));
+  return arr;
+}
+export function parseExpressionWithoutPrecedence(str: string): Expression {
+  function parseArray(expArray: ExpressionArray): Expression {
+    if (typeof expArray === "number") {
+      return expArray;
+    }
+    if (expArray.length === 1) {
+      if (typeof expArray[0] === "string") {
+        throw "Single operator found";
+      }
+      return parseArray(expArray[0]);
+    }
+    const lastOperator = expArray[expArray.length - 2];
+    if (lastOperator === "*" || lastOperator === "+") {
+      return {
+        operator: lastOperator,
+        lhs: parseArray(expArray.slice(0, expArray.length - 2)),
+        rhs: parseArray(expArray.slice(expArray.length - 1)),
+      };
+    }
+    throw `Expected operator, but found ${lastOperator} for ${expArray}`;
+  }
+  return parseArray(parseExpressionArray(str));
+}
+
+export function parseExpressionWithInvertedPrecedence(str: string): Expression {
+  function parseArray(expArray: ExpressionArray): Expression {
+    if (typeof expArray === "number") {
+      return expArray;
+    }
+    if (expArray.length === 1) {
+      if (typeof expArray[0] === "string") {
+        throw "Single operator found";
+      }
+      return parseArray(expArray[0]);
+    }
+    const indexOfMultiplication = expArray.lastIndexOf("*");
+    if (indexOfMultiplication >= 0) {
+      return {
+        operator: "*",
+        lhs: parseArray(expArray.slice(0, indexOfMultiplication)),
+        rhs: parseArray(expArray.slice(indexOfMultiplication + 1)),
+      };
+    } else
+      return {
+        operator: "+",
+        lhs: parseArray(expArray.slice(0, expArray.length - 2)),
+        rhs: parseArray(expArray.slice(expArray.length - 1)),
+      };
+  }
+  return parseArray(parseExpressionArray(str));
 }
 
 export function evaluateExpression(exp: Expression): number {
@@ -54,7 +116,16 @@ export function solution1() {
   const inputLines = getInputArray({ day: 18, year: 2020, separator: "\n" });
   let sum = 0;
   inputLines.forEach((line: string) => {
-    const expression = parseExpression(line);
+    const expression = parseExpressionWithoutPrecedence(line);
+    sum += evaluateExpression(expression);
+  });
+  return sum;
+}
+export function solution2() {
+  const inputLines = getInputArray({ day: 18, year: 2020, separator: "\n" });
+  let sum = 0;
+  inputLines.forEach((line: string) => {
+    const expression = parseExpressionWithInvertedPrecedence(line);
     sum += evaluateExpression(expression);
   });
   return sum;
