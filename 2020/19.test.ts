27caf4c10de1f4205877d5cd6bc230abfd328d95
Benjamin Hogl <benjamin.hogl@jambit.com>
2020-12-19 09:48:49 +0100

    Day 19, part 2, ~45min

--- 2020/19.test.ts
+++ 2020/19.test.ts
@@ -1,4 +1,9 @@
-import { countValidMessages, parseRules, transformRulesToRegexp } from "./19";
+import {
+  isMessageValid1,
+  isMessageValid2,
+  parseRules,
+  transformRulesToRegexps,
+} from "./19";
 
 const ruleString1 = `
 0: 1 2
@@ -16,6 +21,40 @@ const ruleString2 = `
 5: "b"
 `.trim();
 
+const ruleString3 = `
+42: 9 14 | 10 1
+9: 14 27 | 1 26
+10: 23 14 | 28 1
+1: "a"
+11: 42 31
+5: 1 14 | 15 1
+19: 14 1 | 14 14
+12: 24 14 | 19 1
+16: 15 1 | 14 14
+31: 14 17 | 1 13
+6: 14 14 | 1 14
+2: 1 24 | 14 4
+0: 8 11
+13: 14 3 | 1 12
+15: 1 | 14
+17: 14 2 | 1 7
+23: 25 1 | 22 14
+28: 16 1
+4: 1 1
+20: 14 14 | 1 15
+3: 5 14 | 16 1
+27: 1 6 | 14 18
+14: "b"
+21: 14 1 | 1 14
+25: 1 1 | 1 14
+22: 14 14
+8: 42
+26: 14 22 | 1 20
+18: 15 15
+7: 14 5 | 1 21
+24: 14 1
+`.trim();
+
 test("parseRules", () => {
   expect(parseRules(ruleString1)).toEqual({
     "0": "1 2",
@@ -25,14 +64,36 @@ test("parseRules", () => {
   });
 });
 
-test("transformRulesToRegexp", () => {
-  expect(transformRulesToRegexp(parseRules(ruleString1))).toEqual(
-    /^(a)((a)(b)|(b)(a))$/
+test("transformRulesToRegexps", () => {
+  expect(transformRulesToRegexps(parseRules(ruleString1))["0"]).toEqual(
+    "(a(ab|ba))"
   );
 });
 
-test("countValidMessages", () => {
+test("isMessageValid1", () => {
   const messages = ["ababbb", "bababa", "abbbab", "aaabbb", "aaaabbb"];
-  const regexp = transformRulesToRegexp(parseRules(ruleString2));
-  expect(countValidMessages(messages, regexp)).toBe(2);
+  const regexps = transformRulesToRegexps(parseRules(ruleString2));
+  expect(messages.filter((m) => isMessageValid1(m, regexps))).toHaveLength(2);
+});
+test("isMessageValid2", () => {
+  const messages = [
+    "abbbbbabbbaaaababbaabbbbabababbbabbbbbbabaaaa",
+    "bbabbbbaabaabba",
+    "babbbbaabbbbbabbbbbbaabaaabaaa",
+    "aaabbbbbbaaaabaababaabababbabaaabbababababaaa",
+    "bbbbbbbaaaabbbbaaabbabaaa",
+    "bbbababbbbaaaaaaaabbababaaababaabab",
+    "ababaaaaaabaaab",
+    "ababaaaaabbbaba",
+    "baabbaaaabbaaaababbaababb",
+    "abbbbabbbbaaaababbbbbbaaaababb",
+    "aaaaabbaabaaaaababaa",
+    "aaaabbaaaabbaaa",
+    "aaaabbaabbaaaaaaabbbabbbaaabbaabaaa",
+    "babaaabbbaaabaababbaabababaaab",
+    "aabbbbbaabbbaaaaaabbbbbababaaaaabbaaabba",
+  ];
+  const regexps = transformRulesToRegexps(parseRules(ruleString3));
+  expect(messages.filter((m) => isMessageValid1(m, regexps))).toHaveLength(3);
+  expect(messages.filter((m) => isMessageValid2(m, regexps))).toHaveLength(12);
 });
--- 2020/19.ts
+++ 2020/19.ts
@@ -1,6 +1,8 @@
 import { getInputArray } from "../utils";
 
-export function parseRules(ruleString: string): { [key: string]: string } {
+type RuleObject = { [key: string]: string };
+
+export function parseRules(ruleString: string): RuleObject {
   const ruleMap = {};
   ruleString.split("\n").forEach((line) => {
     const [ruleNumber, rule] = line.split(": ");
@@ -9,9 +11,7 @@ export function parseRules(ruleString: string): { [key: string]: string } {
   return ruleMap;
 }
 
-export function transformRulesToRegexp(rules: {
-  [key: string]: string;
-}): RegExp {
+export function transformRulesToRegexps(rules: RuleObject): RuleObject {
   const regexpContents = {};
   function getRegexpContent(ruleNumber: string) {
     if (regexpContents[ruleNumber]) {
@@ -24,36 +24,64 @@ export function transformRulesToRegexp(rules: {
       }
       return rule[1];
     }
-    return rule
-      .split(" | ")
-      .map((subrule) =>
-        subrule
-          .split(" ")
-          .map((ruleNumber) => `(${getRegexpContent(ruleNumber)})`)
-          .join("")
-      )
-      .join("|");
+    return (
+      "(" +
+      rule
+        .split(" | ")
+        .map((subrule) =>
+          subrule
+            .split(" ")
+            .map((ruleNumber) => getRegexpContent(ruleNumber))
+            .join("")
+        )
+        .join("|") +
+      ")"
+    );
   }
-  return new RegExp(`^${getRegexpContent("0")}$`);
+  return Object.keys(rules).reduce((result, ruleNumber) => {
+    result[ruleNumber] = getRegexpContent(ruleNumber);
+    return result;
+  }, {});
 }
 
-export function countValidMessages(messages: string[], regexp: RegExp): number {
-  let count = 0;
-  for (const message of messages) {
-    if (message.match(regexp)) {
-      count++;
-    }
+export function isMessageValid1(
+  message: string,
+  regexpStrings: RuleObject
+): boolean {
+  return new RegExp(`^${regexpStrings[0]}$`).test(message);
+}
+export function isMessageValid2(
+  message: string,
+  regexpStrings: RuleObject
+): boolean {
+  const regexpAll = new RegExp(
+    `^(?<first>(${regexpStrings["42"]}){2,})(?<last>(${regexpStrings["31"]}){1,})$`
+  );
+  const matchAll = message.match(regexpAll);
+  if (!matchAll) {
+    return false;
   }
-  return count;
+  const part42 = matchAll.groups.first;
+  const part31 = matchAll.groups.last;
+  const matches42 = part42.replace(new RegExp(regexpStrings["42"], "g"), ".");
+  const matches31 = part31.replace(new RegExp(regexpStrings["31"], "g"), ".");
+
+  return matches42.length > matches31.length;
 }
 
+function getInput() {
+  return getInputArray({ day: 19, year: 2020, separator: "\n\n" });
+}
 export function solution1() {
-  const [ruleString, messageString] = getInputArray({
-    day: 19,
-    year: 2020,
-    separator: "\n\n",
-  });
-  const regexp = transformRulesToRegexp(parseRules(ruleString));
+  const [ruleString, messageString] = getInput();
+  const regexps = transformRulesToRegexps(parseRules(ruleString));
   const messages = messageString.split("\n");
-  return countValidMessages(messages, regexp);
+  return messages.filter((m) => isMessageValid1(m, regexps)).length;
+}
+
+export function solution2() {
+  const [ruleString, messageString] = getInput();
+  const regexps = transformRulesToRegexps(parseRules(ruleString));
+  const messages = messageString.split("\n");
+  return messages.filter((m) => isMessageValid2(m, regexps)).length;
 }
