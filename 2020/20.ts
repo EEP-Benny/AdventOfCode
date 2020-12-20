b359bc51eb4a8d02b280955a43d1cc162b4ab655
Benjamin Hogl <9270901+EEP-Benny@users.noreply.github.com>
2020-12-20 18:42:05 +0100

    Day 20, part 2, took many hours

--- 2020/20.ts
+++ 2020/20.ts
@@ -41,6 +41,81 @@ export function transformBorderIds<T>(
   }
 }
 
+function transformCoordinates(
+  x: number,
+  y: number,
+  last: number,
+  orientation: Orientation
+) {
+  switch (orientation) {
+    case Orientation.top:
+      return [x, y];
+    case Orientation.topFlipped:
+      return [last - x, y];
+    case Orientation.right:
+      return [y, last - x];
+    case Orientation.rightFlipped:
+      return [last - y, last - x];
+    case Orientation.bottom:
+      return [last - x, last - y];
+    case Orientation.bottomFlipped:
+      return [x, last - y];
+    case Orientation.left:
+      return [last - y, x];
+    case Orientation.leftFlipped:
+      return [y, x];
+    default:
+      return [0, 0];
+  }
+}
+export function transformPixels(pixels: string[], orientation: Orientation) {
+  const transformedPixels: string[] = [];
+  const last = pixels.length - 1;
+  for (let y = 0; y <= last; y++) {
+    const line = [];
+    for (let x = 0; x <= last; x++) {
+      const [x2, y2] = transformCoordinates(x, y, last, orientation);
+      line.push(pixels[y2][x2]);
+    }
+    transformedPixels.push(line.join(""));
+  }
+  return transformedPixels;
+}
+export function transformAndCropPixels(
+  pixels: string[],
+  orientation: Orientation
+) {
+  const croppedPixels = pixels
+    .slice(1, pixels.length - 1)
+    .map((line) => line.substring(1, line.length - 1));
+  return transformPixels(croppedPixels, orientation);
+}
+
+export function makePatternDetector(patternLines: string[]) {
+  const requiredPixels: [number, number][] = [];
+  patternLines.forEach((patternLine, y) => {
+    patternLine.split("").forEach((pixel, x) => {
+      if (pixel !== " ") {
+        requiredPixels.push([x, y]);
+      }
+    });
+  });
+  return function isMatchAtPosition(x: number, y: number, pixels: string[]) {
+    for (const [dx, dy] of requiredPixels) {
+      if (pixels[y + dy]?.[x + dx] !== "#") {
+        return false;
+      }
+    }
+    return true;
+  };
+}
+
+export const seaMonsterPattern = [
+  "                  # ",
+  "#    ##    ##    ###",
+  " #  #  #  #  #  #   ",
+];
+export const isSeaMonsterAtPosition = makePatternDetector(seaMonsterPattern);
 type BorderMap = Map<number, Set<string>>;
 
 export enum Orientation {
@@ -67,12 +142,14 @@ export function stitchRecord(
 export class JigsawPuzzle {
   borderIdToTileIdsMap: BorderMap = new Map();
   tileIdToBorderIdsMap = new Map<string, number[]>();
+  tileIdToPixelsMap = new Map<string, string[]>();
   matchesPerTileId = new Map<string, number>();
 
   constructor(tiles: string[]) {
     for (const tile of tiles) {
       const matches = tile.match(/^Tile (\d+):\n(.*)$/s); // s flag: Allows . to match newline characters
       const [, tileId, pixels] = matches;
+      this.tileIdToPixelsMap.set(tileId, pixels.split("\n"));
       const borderIds = getBorderIds(pixels);
       this.tileIdToBorderIdsMap.set(tileId, borderIds);
       for (const borderId of borderIds) {
@@ -175,10 +252,69 @@ export class JigsawPuzzle {
     } while (firstTileOfNextRow);
     return tileArrangement;
   }
+
+  getStitchedImage(): string[] {
+    const tileArrangement = this.findTileArrangement();
+    const stitchedImage = [];
+    for (const tileRow of tileArrangement) {
+      let row: string[] = undefined;
+      for (const tile of tileRow) {
+        const pixelsOfTile = transformAndCropPixels(
+          this.tileIdToPixelsMap.get(tile.tileId),
+          tile.orientation
+        );
+        if (row === undefined) {
+          row = pixelsOfTile;
+        } else {
+          pixelsOfTile.forEach((pixelLine, lineNumber) => {
+            row[lineNumber] += pixelLine;
+          });
+        }
+      }
+      stitchedImage.push(...row);
+    }
+    return stitchedImage;
+  }
+
+  countSeaMonsters(): number {
+    let maxSeaMonsters = 0;
+    const stitchedImage = this.getStitchedImage();
+    for (const orientation of [0, 1, 2, 3, 4, 5, 6, 7]) {
+      let currentSeaMonsters = 0;
+      const transformedImage = transformPixels(stitchedImage, orientation);
+      for (let y = 0; y < transformedImage.length; y++) {
+        for (let x = 0; x < transformedImage[y].length; x++) {
+          if (isSeaMonsterAtPosition(x, y, transformedImage)) {
+            currentSeaMonsters++;
+          }
+        }
+      }
+      if (currentSeaMonsters > maxSeaMonsters) {
+        maxSeaMonsters = currentSeaMonsters;
+      }
+    }
+    return maxSeaMonsters;
+  }
+
+  getHabitatRoughness() {
+    const stitchedImage = this.getStitchedImage();
+    const numberOfSeaMonsters = this.countSeaMonsters();
+    const numberOfRoughPixels = stitchedImage.join("").match(/#/g).length;
+    const numberOfPixelsPerSeaMonster = seaMonsterPattern.join("").match(/#/g)
+      .length;
+    return (
+      numberOfRoughPixels - numberOfSeaMonsters * numberOfPixelsPerSeaMonster
+    );
+  }
 }
 export function solution1() {
   const tiles = getInputArray({ day: 20, year: 2020, separator: "\n\n" });
   const puzzle = new JigsawPuzzle(tiles);
   const cornerTileIds = puzzle.getCornerTileIds();
   return cornerTileIds.reduce((soFar, tileId) => soFar * +tileId, 1);
+}
+
+export function solution2() {
+  const tiles = getInputArray({ day: 20, year: 2020, separator: "\n\n" });
+  return new JigsawPuzzle(tiles).getHabitatRoughness();
 }
