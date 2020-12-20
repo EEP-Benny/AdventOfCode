8510b7857d1dd9e81c1c8068bbbac82f018e6bd3
Benjamin Hogl <benjamin.hogl@jambit.com>
2020-12-20 16:53:51 +0100

    Day 20, part 2.1, >3h

--- 2020/20.test.ts
+++ 2020/20.test.ts
@@ -1,8 +1,10 @@
 import {
   getBorderIds,
-  getCornerTileIds,
-  getMatchesPerTileId,
+  JigsawPuzzle,
+  Orientation,
   pixelsToIntegers,
+  stitchRecord,
+  transformBorderIds,
 } from "./20";
 
 const exampleTile = `
@@ -136,11 +138,28 @@ test("pixelsToIntegers", () => {
 });
 
 test("getBorderIds", () => {
-  expect(getBorderIds(exampleTile)).toEqual([0, 0, 1, 512, 515, 769, 3, 768]);
+  expect(getBorderIds(exampleTile)).toEqual([0, 0, 1, 512, 769, 515, 768, 3]);
 });
 
-test("getMatchesPerTileId", () => {
-  expect(getMatchesPerTileId(exampleTiles)).toEqual(
+test("transformBorderIds", () => {
+  const borderIds = "ab,ba,bc,cb,cd,dc,da,ad".split(",");
+  expect(transformBorderIds(borderIds, Orientation.top)).toEqual(
+    "ab,ba,bc,cb,cd,dc,da,ad".split(",")
+  );
+  expect(transformBorderIds(borderIds, Orientation.topFlipped)).toEqual(
+    "ba,ab,ad,da,dc,cd,cb,bc".split(",")
+  );
+  expect(transformBorderIds(borderIds, Orientation.right)).toEqual(
+    "da,ad,ab,ba,bc,cb,cd,dc".split(",")
+  );
+  expect(transformBorderIds(borderIds, Orientation.rightFlipped)).toEqual(
+    "cb,bc,ba,ab,ad,da,dc,cd".split(",")
+  );
+});
+
+test("JigsawPuzzle.matchesPerTileId", () => {
+  const puzzle = new JigsawPuzzle(exampleTiles);
+  expect(puzzle.matchesPerTileId).toEqual(
     new Map([
       ["2311", 6],
       ["1951", 4],
@@ -155,11 +174,68 @@ test("getMatchesPerTileId", () => {
   );
 });
 
-test("getCornerTileIds", () => {
-  expect(getCornerTileIds(getMatchesPerTileId(exampleTiles))).toEqual([
-    "3079",
-    "1951",
-    "1171",
-    "2971",
-  ]);
+test("JigsawPuzzle.getCornerTileIds", () => {
+  const puzzle = new JigsawPuzzle(exampleTiles);
+  expect(puzzle.getCornerTileIds()).toEqual(["3079", "1951", "1171", "2971"]);
+});
+
+test("JigsawPuzzle.get*Neighbor", () => {
+  const puzzle = new JigsawPuzzle(exampleTiles);
+  const sr = stitchRecord("3079", Orientation.top);
+  const sr90 = stitchRecord("3079", Orientation.right);
+  const sr180 = stitchRecord("3079", Orientation.bottom);
+  const srF = stitchRecord("3079", Orientation.topFlipped);
+
+  expect(puzzle.getTopNeighbor(sr)).toEqual(undefined);
+  expect(puzzle.getRightNeighbor(sr)).toEqual(undefined);
+  expect(puzzle.getBottomNeighbor(sr)).toEqual(
+    stitchRecord("2473", Orientation.rightFlipped)
+  );
+  expect(puzzle.getLeftNeighbor(sr)).toEqual(
+    stitchRecord("2311", Orientation.bottomFlipped)
+  );
+  expect(puzzle.getTopNeighbor(sr90)).toEqual(
+    stitchRecord("2311", Orientation.leftFlipped)
+  );
+  expect(puzzle.getRightNeighbor(sr90)).toEqual(undefined);
+  expect(puzzle.getBottomNeighbor(sr90)).toEqual(undefined);
+  expect(puzzle.getLeftNeighbor(sr90)).toEqual(
+    stitchRecord("2473", Orientation.bottomFlipped)
+  );
+  expect(puzzle.getTopNeighbor(sr180)).toEqual(
+    stitchRecord("2473", Orientation.leftFlipped)
+  );
+  expect(puzzle.getRightNeighbor(sr180)).toEqual(
+    stitchRecord("2311", Orientation.topFlipped)
+  );
+  expect(puzzle.getBottomNeighbor(sr180)).toEqual(undefined);
+  expect(puzzle.getLeftNeighbor(sr180)).toEqual(undefined);
+  expect(puzzle.getTopNeighbor(srF)).toEqual(undefined);
+  expect(puzzle.getRightNeighbor(srF)).toEqual(
+    stitchRecord("2311", Orientation.bottom)
+  );
+  expect(puzzle.getBottomNeighbor(srF)).toEqual(
+    stitchRecord("2473", Orientation.left)
+  );
+  expect(puzzle.getLeftNeighbor(srF)).toEqual(undefined);
+});
+
+test("JigsawPuzzle.getTopLeftTile", () => {
+  const puzzle = new JigsawPuzzle(exampleTiles);
+  expect(puzzle.getTopLeftTile()).toEqual(
+    stitchRecord("3079", Orientation.left)
+  );
+});
+
+test("JigsawPuzzle.findTileArrangement", () => {
+  const stitchedTiles = new JigsawPuzzle(exampleTiles).findTileArrangement();
+  expect(stitchedTiles[0][0]?.tileId).toBe("3079");
+  expect(stitchedTiles[0][1]?.tileId).toBe("2473");
+  expect(stitchedTiles[0][2]?.tileId).toBe("1171");
+  expect(stitchedTiles[1][0]?.tileId).toBe("2311");
+  expect(stitchedTiles[1][1]?.tileId).toBe("1427");
+  expect(stitchedTiles[1][2]?.tileId).toBe("1489");
+  expect(stitchedTiles[2][0]?.tileId).toBe("1951");
+  expect(stitchedTiles[2][1]?.tileId).toBe("2729");
+  expect(stitchedTiles[2][2]?.tileId).toBe("2971");
 });
