9c696ef185d29d72d733f700195903b0dbe47f4c
Benjamin Hogl <benjamin.hogl@jambit.com>
2020-12-22 08:32:57 +0100

    Day 22, part 1, ~30min

---
+++ 2020/22.test.ts
@@ -0,0 +1,51 @@
+import { info } from "console";
+import { CombatGame } from "./22";
+
+const input = `
+Player 1:
+9
+2
+6
+3
+1
+
+Player 2:
+5
+8
+4
+7
+10
+`.trim();
+
+test("CombatGame.constructor", () => {
+  const game = new CombatGame(input);
+  expect(game.player1Cards).toEqual([9, 2, 6, 3, 1]);
+  expect(game.player2Cards).toEqual([5, 8, 4, 7, 10]);
+});
+
+test("CombatGame.playSingleRound", () => {
+  const game = new CombatGame(input);
+  game.playSingleRound();
+  expect(game.roundCounter).toEqual(1);
+  expect(game.player1Cards).toEqual([2, 6, 3, 1, 9, 5]);
+  expect(game.player2Cards).toEqual([8, 4, 7, 10]);
+  game.playSingleRound();
+  game.playSingleRound();
+  game.playSingleRound();
+  expect(game.roundCounter).toEqual(4);
+  expect(game.player1Cards).toEqual([1, 9, 5, 6, 4]);
+  expect(game.player2Cards).toEqual([10, 8, 2, 7, 3]);
+});
+
+test("CombatGame.playUntilWin", () => {
+  const game = new CombatGame(input);
+  game.playUntilWin();
+  expect(game.roundCounter).toEqual(29);
+  expect(game.player1Cards).toEqual([]);
+  expect(game.player2Cards).toEqual([3, 2, 10, 6, 8, 5, 9, 4, 7, 1]);
+});
+
+test("CombatGame.getWinningScore", () => {
+  const game = new CombatGame(input);
+  expect(game.getWinningScore()).toEqual(306);
+});
