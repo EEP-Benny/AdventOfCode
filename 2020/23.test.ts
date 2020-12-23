adc0aac3b734874c0a1ed25a62e6c193e85d5f24
Benjamin Hogl <benjamin.hogl@jambit.com>
2020-12-23 16:39:08 +0100

    Day 23, part2: Optimized a lot, still not fast enough

--- 2020/23.test.ts
+++ 2020/23.test.ts
@@ -3,24 +3,46 @@ import { CupGame } from "./23";
 test("CupGame", () => {
   const game = new CupGame("32415");
   expect(game.cups).toEqual([3, 2, 4, 1, 5]);
+  expect(game.lowestCup).toEqual(1);
+  expect(game.highestCup).toEqual(5);
 });
 
 test("CupGame.play", () => {
   const game = new CupGame("389125467");
+  expect(game.lowestCup).toEqual(1);
+  expect(game.highestCup).toEqual(9);
+
   expect(game.roundCounter).toEqual(0);
+  expect(game.currentCupIndex).toEqual(0);
   expect(game.cups).toEqual([3, 8, 9, 1, 2, 5, 4, 6, 7]);
   game.playSingleRound();
   expect(game.roundCounter).toEqual(1);
-  expect(game.cups).toEqual([2, 8, 9, 1, 5, 4, 6, 7, 3]);
+  expect(game.currentCupIndex).toEqual(1);
+  expect(game.cups).toEqual([3, 2, 8, 9, 1, 5, 4, 6, 7]);
   game.playSingleRound();
   expect(game.roundCounter).toEqual(2);
-  expect(game.cups).toEqual([5, 4, 6, 7, 8, 9, 1, 3, 2]);
+  expect(game.currentCupIndex).toEqual(5);
+  expect(game.cups).toEqual([8, 9, 1, 3, 2, 5, 4, 6, 7]);
   game.playSingleRound();
   expect(game.roundCounter).toEqual(3);
+  expect(game.currentCupIndex).toEqual(9);
   expect(game.cups).toEqual([8, 9, 1, 3, 4, 6, 7, 2, 5]);
+  // game.playSingleRound();
+  // expect(game.roundCounter).toEqual(4);
+  // expect(game.currentCupIndex).toEqual(4);
+  // expect(game.cups).toEqual([3, 2, 5, 8, 4, 6, 7, 9, 1]);
+  // game.playSingleRound();
+  // expect(game.roundCounter).toEqual(5);
+  // expect(game.currentCupIndex).toEqual(5);
+  // expect(game.cups).toEqual([9, 2, 5, 8, 4, 1, 3, 6, 7]);
+  // game.playSingleRound();
+  // expect(game.roundCounter).toEqual(6);
+  // expect(game.currentCupIndex).toEqual(6);
+  // expect(game.cups).toEqual([7, 2, 5, 8, 4, 1, 9, 3, 6]);
   game.playUntilRound(10);
   expect(game.roundCounter).toEqual(10);
-  expect(game.cups).toEqual([8, 3, 7, 4, 1, 9, 2, 6, 5]);
+  expect(game.currentCupIndex).toEqual(31);
+  expect(game.cups).toEqual([9, 2, 6, 5, 8, 3, 7, 4, 1]);
 });
 
 test("CupGame.getCupLabels", () => {
--- 2020/23.ts
+++ 2020/23.ts
@@ -2,6 +2,7 @@ type Cup = number;
 export class CupGame {
   // convention: the current cup is always at position 0
   cups: Cup[];
+  currentCupIndex = 0;
   lowestCup: Cup;
   highestCup: Cup;
   roundCounter = 0;
@@ -18,29 +19,66 @@ export class CupGame {
     }
   }
 
+  setCup(index: number, value: Cup) {
+    this.cups[index % this.cups.length] = value;
+  }
+  getCup(index: number): Cup {
+    return this.cups[index % this.cups.length];
+  }
+
   playSingleRound() {
-    let destinationCup = this.cups[0];
-    let destinationCupIndex = -1;
-    // search for a cup that isn't picked up (cups 1-3 are picked up)
-    while (destinationCupIndex === -1) {
+    const currentCup = this.getCup(this.currentCupIndex);
+    const pickedUpCups = [1, 2, 3].map((i) =>
+      this.getCup(this.currentCupIndex + i)
+    );
+
+    let destinationCup = currentCup;
+    while (
+      pickedUpCups.includes(destinationCup) ||
+      destinationCup === currentCup
+    ) {
       destinationCup--;
       if (destinationCup < this.lowestCup) destinationCup = this.highestCup;
-      destinationCupIndex = this.cups.indexOf(destinationCup, 4);
+    }
+    let destinationCupIndex = this.cups.indexOf(destinationCup);
+    if (destinationCupIndex < 0) {
+      throw "couldn't find destination cup " + destinationCup;
+    }
+    while (destinationCupIndex < this.currentCupIndex - this.cups.length / 2) {
+      destinationCupIndex += this.cups.length;
+    }
+    while (destinationCupIndex > this.currentCupIndex + this.cups.length / 2) {
+      destinationCupIndex -= this.cups.length;
     }
 
-    this.cups = this.cups.map((_, i) => {
-      if (i <= destinationCupIndex - 4) {
-        return this.cups[i + 4];
-      }
-      if (i <= destinationCupIndex - 1) {
-        return this.cups[i + 4 - destinationCupIndex];
-      }
-      if (i <= this.cups.length - 2) {
-        return this.cups[i + 1];
-      } else {
-        return this.cups[0];
-      }
+    console.log({
+      round: this.roundCounter,
+      difference: this.currentCupIndex - destinationCupIndex,
+      currentIndex: this.currentCupIndex,
+      destinationIndex: destinationCupIndex,
+      current: currentCup,
+      destination: destinationCup,
     });
+    if (this.currentCupIndex < destinationCupIndex) {
+      // shift forward
+      for (let i = this.currentCupIndex + 4; i <= destinationCupIndex; i++) {
+        this.setCup(i - 3, this.getCup(i));
+      }
+      for (let i = 0; i < 3; i++) {
+        this.setCup(destinationCupIndex - 2 + i, pickedUpCups[i]);
+      }
+    } else {
+      // shift backward
+      for (let i = this.currentCupIndex; i >= destinationCupIndex + 1; i--) {
+        this.setCup(i + 3, this.getCup(i));
+      }
+      for (let i = 0; i < 3; i++) {
+        this.setCup(destinationCupIndex + 1 + i, pickedUpCups[i]);
+      }
+      this.currentCupIndex += 3;
+    }
+
+    this.currentCupIndex++;
     this.roundCounter++;
   }
 
@@ -60,11 +98,13 @@ export class CupGame {
 
 export function solution1() {
   const game = new CupGame("942387615");
-  game.playUntilRound(100);
+  // game.playUntilRound(100);
   return game.getCupLabels();
 }
 export function solution2() {
   const game = new CupGame("942387615", true);
-  game.playUntilRound(1000);
+  game.playUntilRound(20);
+  console.log(game.cups.slice(0, 100));
+
   // return game.getCupLabels();
 }
