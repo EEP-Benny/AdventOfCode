4633b5f228828bb079e295dd6f9f23b7e8fa4103
Benjamin Hogl <9270901+EEP-Benny@users.noreply.github.com>
2021-12-07 08:33:27 +0100

    Day 7, part 2, ~8min

--- 2021/07/07.go
+++ 2021/07/07.go
@@ -8,11 +8,11 @@ import (
 
 func main() {
 	input := utils.LoadInputSliceInt(2021, 7, ",")
-	fmt.Println("Solution 1:", getLowestFuelCost(input))
-	// fmt.Println("Solution 2:", ???)
+	fmt.Println("Solution 1:", getLowestFuelCost(input, calculateLinearFuelCost))
+	fmt.Println("Solution 2:", getLowestFuelCost(input, calculateQuadraticFuelCost))
 }
 
-func calculateFuelCost(startPositions []int, targetPosition int) int {
+func calculateLinearFuelCost(startPositions []int, targetPosition int) int {
 	fuelCost := 0
 	for _, startPosition := range startPositions {
 		fuelCost += utils.Abs(startPosition - targetPosition)
@@ -20,11 +20,20 @@ func calculateFuelCost(startPositions []int, targetPosition int) int {
 	return fuelCost
 }
 
-func getLowestFuelCost(startPositions []int) int {
+func calculateQuadraticFuelCost(startPositions []int, targetPosition int) int {
+	fuelCost := 0
+	for _, startPosition := range startPositions {
+		distance := utils.Abs(startPosition - targetPosition)
+		fuelCost += (distance * (distance + 1)) / 2
+	}
+	return fuelCost
+}
+
+func getLowestFuelCost(startPositions []int, fuelCostFn func([]int, int) int) int {
 	lowest, highest := utils.MinMax(startPositions)
-	lowestFuelCost := calculateFuelCost(startPositions, lowest)
+	lowestFuelCost := fuelCostFn(startPositions, lowest)
 	for targetPosition := lowest + 1; targetPosition <= highest; targetPosition++ {
-		fuelCost := calculateFuelCost(startPositions, targetPosition)
+		fuelCost := fuelCostFn(startPositions, targetPosition)
 		if fuelCost < lowestFuelCost {
 			lowestFuelCost = fuelCost
 		}
--- 2021/07/07_test.go
+++ 2021/07/07_test.go
@@ -6,7 +6,7 @@ import (
 
 var exampleInput = []int{16, 1, 2, 0, 4, 2, 7, 1, 2, 14}
 
-func Test_calculateFuelCost(t *testing.T) {
+func Test_calculateLinearFuelCost(t *testing.T) {
 	type args struct {
 		startPositions []int
 		targetPosition int
@@ -23,8 +23,30 @@ func Test_calculateFuelCost(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := calculateFuelCost(tt.args.startPositions, tt.args.targetPosition); got != tt.want {
-				t.Errorf("calculateFuelCost() = %v, want %v", got, tt.want)
+			if got := calculateLinearFuelCost(tt.args.startPositions, tt.args.targetPosition); got != tt.want {
+				t.Errorf("calculateLinearFuelCost() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_calculateQuadraticFuelCost(t *testing.T) {
+	type args struct {
+		startPositions []int
+		targetPosition int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"example input -> 2", args{exampleInput, 2}, 206},
+		{"example input -> 5", args{exampleInput, 5}, 168},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := calculateQuadraticFuelCost(tt.args.startPositions, tt.args.targetPosition); got != tt.want {
+				t.Errorf("calculateQuadraticFuelCost() = %v, want %v", got, tt.want)
 			}
 		})
 	}
@@ -33,17 +55,19 @@ func Test_calculateFuelCost(t *testing.T) {
 func Test_getLowestFuelCost(t *testing.T) {
 	type args struct {
 		startPositions []int
+		fuelCostFn     func([]int, int) int
 	}
 	tests := []struct {
 		name string
 		args args
 		want int
 	}{
-		{"example input", args{exampleInput}, 37},
+		{"example input with linear cost", args{exampleInput, calculateLinearFuelCost}, 37},
+		{"example input with quadratic cost", args{exampleInput, calculateQuadraticFuelCost}, 168},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := getLowestFuelCost(tt.args.startPositions); got != tt.want {
+			if got := getLowestFuelCost(tt.args.startPositions, tt.args.fuelCostFn); got != tt.want {
 				t.Errorf("getLowestFuelCost() = %v, want %v", got, tt.want)
 			}
 		})
