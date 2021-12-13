803a30c0f09547321ee34c95ef6f375ccc7833a3
Benjamin Hogl <9270901+EEP-Benny@users.noreply.github.com>
2021-12-13 08:55:49 +0100

    Day 13, part 1, ~50min

---
+++ 2021/13/13.go
@@ -0,0 +1,113 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/EEP-Benny/AdventOfCode/utils"
+)
+
+type Fold = struct {
+	isVertical bool
+	position   int
+}
+
+type DotMatrix = [][]bool
+
+func main() {
+	dotPositions, folds := processInput(utils.LoadInput(2021, 13))
+	dotMatrix := createDotMatrix(dotPositions)
+	fmt.Println("Solution 1:", countDots(executeFold(dotMatrix, folds[0])))
+	// fmt.Println("Solution 2:", ???)
+}
+
+func processInput(inputString string) (dotPositions [][]int, folds []Fold) {
+	splitInputString := strings.Split(inputString, "\n\n")
+	dotPositionsString := splitInputString[0]
+	foldsString := splitInputString[1]
+
+	for _, dotPositionString := range strings.Split(dotPositionsString, "\n") {
+		dotPositions = append(dotPositions, utils.IntSlice(strings.Split(dotPositionString, ",")))
+	}
+
+	for _, foldString := range strings.Split(foldsString, "\n") {
+		splitFoldString := strings.Split(foldString, "=")
+		folds = append(folds, Fold{
+			isVertical: splitFoldString[0] == "fold along x",
+			position:   utils.StringToInt(splitFoldString[1]),
+		})
+	}
+
+	return dotPositions, folds
+}
+
+func createDotMatrix(dotPositions [][]int) DotMatrix {
+	xPositions := make([]int, len(dotPositions))
+	yPositions := make([]int, len(dotPositions))
+	for i, dotPosition := range dotPositions {
+		xPositions[i] = dotPosition[0]
+		yPositions[i] = dotPosition[1]
+	}
+	_, maxX := utils.MinMax(xPositions)
+	_, maxY := utils.MinMax(yPositions)
+	dotMatrix := make(DotMatrix, maxY+1)
+	for y := 0; y < maxY+1; y++ {
+		dotMatrix[y] = make([]bool, maxX+1)
+	}
+
+	for i := 0; i < len(dotPositions); i++ {
+		dotMatrix[yPositions[i]][xPositions[i]] = true
+	}
+
+	return dotMatrix
+}
+
+func executeFold(dotMatrix DotMatrix, fold Fold) DotMatrix {
+	newSizeX := len(dotMatrix[0])
+	newSizeY := len(dotMatrix)
+	if fold.isVertical {
+		newSizeX = fold.position
+	} else {
+		newSizeY = fold.position
+	}
+
+	newDotMatrix := make(DotMatrix, newSizeY)
+	for y := 0; y < newSizeY; y++ {
+		newDotMatrix[y] = make([]bool, newSizeX)
+		for x := 0; x < newSizeX; x++ {
+			newDotMatrix[y][x] = dotMatrix[y][x]
+		}
+	}
+
+	if fold.isVertical {
+		for y := 0; y < newSizeY; y++ {
+			for x := newSizeX + 1; x < len(dotMatrix[y]); x++ {
+				if dotMatrix[y][x] {
+					newDotMatrix[y][len(dotMatrix[y])-x-1] = true
+				}
+			}
+		}
+	} else {
+		for y := newSizeY + 1; y < len(dotMatrix); y++ {
+			for x := 0; x < newSizeX; x++ {
+				if dotMatrix[y][x] {
+					newDotMatrix[len(dotMatrix)-y-1][x] = true
+				}
+			}
+		}
+	}
+
+	return newDotMatrix
+}
+
+func countDots(dotMatrix DotMatrix) int {
+	count := 0
+	for _, row := range dotMatrix {
+		for _, cell := range row {
+			if cell {
+				count++
+			}
+		}
+	}
+	return count
+}
---
+++ 2021/13/13_test.go
@@ -0,0 +1,171 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+var exampleInput = `6,10
+0,14
+9,10
+0,3
+10,4
+4,11
+6,0
+6,12
+4,1
+0,13
+10,12
+3,4
+3,0
+8,4
+1,10
+2,14
+8,10
+9,0
+
+fold along y=7
+fold along x=5`
+
+var exampleDotPositions, exampleFolds = processInput(exampleInput)
+
+func Test_processInput(t *testing.T) {
+	type args struct {
+		inputString string
+	}
+	tests := []struct {
+		name             string
+		args             args
+		wantDotPositions [][]int
+		wantFolds        []Fold
+	}{
+		{"exampleInput", args{exampleInput}, [][]int{
+			{6, 10},
+			{0, 14},
+			{9, 10},
+			{0, 3},
+			{10, 4},
+			{4, 11},
+			{6, 0},
+			{6, 12},
+			{4, 1},
+			{0, 13},
+			{10, 12},
+			{3, 4},
+			{3, 0},
+			{8, 4},
+			{1, 10},
+			{2, 14},
+			{8, 10},
+			{9, 0},
+		}, []Fold{
+			{isVertical: false, position: 7},
+			{isVertical: true, position: 5},
+		}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gotDotPositions, gotFolds := processInput(tt.args.inputString)
+			if !reflect.DeepEqual(gotDotPositions, tt.wantDotPositions) {
+				t.Errorf("processInput() gotDotPositions = %v, want %v", gotDotPositions, tt.wantDotPositions)
+			}
+			if !reflect.DeepEqual(gotFolds, tt.wantFolds) {
+				t.Errorf("processInput() gotFolds = %v, want %v", gotFolds, tt.wantFolds)
+			}
+		})
+	}
+}
+
+func Test_createDotMatrix(t *testing.T) {
+	type args struct {
+		dotPositions [][]int
+	}
+	tests := []struct {
+		name string
+		args args
+		want DotMatrix
+	}{
+		{"exampleInput", args{exampleDotPositions}, DotMatrix{
+			{false, false, false, true, false, false, true, false, false, true, false},
+			{false, false, false, false, true, false, false, false, false, false, false},
+			{false, false, false, false, false, false, false, false, false, false, false},
+			{true, false, false, false, false, false, false, false, false, false, false},
+			{false, false, false, true, false, false, false, false, true, false, true},
+			{false, false, false, false, false, false, false, false, false, false, false},
+			{false, false, false, false, false, false, false, false, false, false, false},
+			{false, false, false, false, false, false, false, false, false, false, false},
+			{false, false, false, false, false, false, false, false, false, false, false},
+			{false, false, false, false, false, false, false, false, false, false, false},
+			{false, true, false, false, false, false, true, false, true, true, false},
+			{false, false, false, false, true, false, false, false, false, false, false},
+			{false, false, false, false, false, false, true, false, false, false, true},
+			{true, false, false, false, false, false, false, false, false, false, false},
+			{true, false, true, false, false, false, false, false, false, false, false},
+		}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := createDotMatrix(tt.args.dotPositions); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("createDotMatrix() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_executeFold(t *testing.T) {
+	type args struct {
+		dotMatrix DotMatrix
+		fold      Fold
+	}
+	tests := []struct {
+		name string
+		args args
+		want DotMatrix
+	}{
+		{"example fold 1", args{createDotMatrix(exampleDotPositions), exampleFolds[0]}, DotMatrix{
+			{true, false, true, true, false, false, true, false, false, true, false},
+			{true, false, false, false, true, false, false, false, false, false, false},
+			{false, false, false, false, false, false, true, false, false, false, true},
+			{true, false, false, false, true, false, false, false, false, false, false},
+			{false, true, false, true, false, false, true, false, true, true, true},
+			{false, false, false, false, false, false, false, false, false, false, false},
+			{false, false, false, false, false, false, false, false, false, false, false},
+		}},
+		{"example fold 2", args{executeFold(createDotMatrix(exampleDotPositions), exampleFolds[0]), exampleFolds[1]}, DotMatrix{
+			{true, true, true, true, true},
+			{true, false, false, false, true},
+			{true, false, false, false, true},
+			{true, false, false, false, true},
+			{true, true, true, true, true},
+			{false, false, false, false, false},
+			{false, false, false, false, false},
+		}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := executeFold(tt.args.dotMatrix, tt.args.fold); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("executeFold() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_countDots(t *testing.T) {
+	type args struct {
+		dotMatrix DotMatrix
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"example after fold 1", args{executeFold(createDotMatrix(exampleDotPositions), exampleFolds[0])}, 17},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := countDots(tt.args.dotMatrix); got != tt.want {
+				t.Errorf("countDots() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
