c3a2bd29c09dac45d6717c025abe2d9350777e8b
Benjamin Hogl <9270901+EEP-Benny@users.noreply.github.com>
2021-12-17 09:21:15 +0100

    Day 17, part 2, ~15min

--- 2021/17/17.go
+++ 2021/17/17.go
@@ -18,7 +18,7 @@ type State struct {
 func main() {
 	input := parseInput(utils.LoadInput(2021, 17))
 	fmt.Println("Solution 1:", findHighestPointInTrajectoryThatReachesTheTarget(input))
-	// fmt.Println("Solution 2:", ???)
+	fmt.Println("Solution 2:", countDistinctInitialVelocitiesThatReachTheTarget(input))
 }
 
 func parseInput(inputString string) TargetArea {
@@ -47,7 +47,7 @@ func step(oldState State) State {
 	return newState
 }
 
-func findHighestPointInTrajectoryThatReachesTheTarget(targetArea TargetArea) int {
+func findMinimumXVelocity(targetArea TargetArea) int {
 	// work backwards to find required x velocity to reach target
 	requiredXVelocity := 0
 	reachableXPosition := 0
@@ -55,6 +55,11 @@ func findHighestPointInTrajectoryThatReachesTheTarget(targetArea TargetArea) int
 		requiredXVelocity++
 		reachableXPosition += requiredXVelocity
 	}
+	return requiredXVelocity
+}
+
+func findHighestPointInTrajectoryThatReachesTheTarget(targetArea TargetArea) int {
+	requiredXVelocity := findMinimumXVelocity(targetArea)
 
 	// yPositions are symmetric during rise and fall. This means that the probe will eventually reach posY=0 with vY=-initialVY, and must then land in the targetArea in the next step.
 	maxYVelocity := -targetArea.yMin - 1
@@ -64,4 +69,33 @@ func findHighestPointInTrajectoryThatReachesTheTarget(targetArea TargetArea) int
 	}
 
 	return maxHeight
+}
+
+func willReachTargetArea(initialXVelocity, initialYVelocity int, targetArea TargetArea) bool {
+	for state := (State{posX: 0, posY: 0, vX: initialXVelocity, vY: initialYVelocity}); true; state = step(state) {
+		if state.posX >= targetArea.xMin && state.posX <= targetArea.xMax && state.posY >= targetArea.yMin && state.posY <= targetArea.yMax {
+			return true
+		}
+		if state.posX > targetArea.xMax || state.posY < targetArea.yMin {
+			break
+		}
+	}
+	return false
+}
+
+func countDistinctInitialVelocitiesThatReachTheTarget(targetArea TargetArea) int {
+	initialVelocitiesThatReachTheTarget := make(map[[2]int]bool)
+	minVX := findMinimumXVelocity(targetArea)
+	maxVX := targetArea.xMax
+	minVY := targetArea.yMin
+	maxVY := -targetArea.yMin - 1 // see above
+
+	for vX := minVX; vX <= maxVX; vX++ {
+		for vY := minVY; vY <= maxVY; vY++ {
+			if willReachTargetArea(vX, vY, targetArea) {
+				initialVelocitiesThatReachTheTarget[[2]int{vX, vY}] = true
+			}
+		}
+	}
+	return len(initialVelocitiesThatReachTheTarget)
 }
--- 2021/17/17_test.go
+++ 2021/17/17_test.go
@@ -67,3 +67,48 @@ func Test_findHighestPointInTrajectoryThatReachesTheTarget(t *testing.T) {
 		})
 	}
 }
+
+func Test_willReachTargetArea(t *testing.T) {
+	type args struct {
+		initialXVelocity int
+		initialYVelocity int
+		targetArea       TargetArea
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{"example that reaches target (1)", args{7, 2, TargetArea{xMin: 20, xMax: 30, yMin: -10, yMax: -5}}, true},
+		{"example that reaches target (2)", args{6, 3, TargetArea{xMin: 20, xMax: 30, yMin: -10, yMax: -5}}, true},
+		{"example that reaches target (3)", args{9, 0, TargetArea{xMin: 20, xMax: 30, yMin: -10, yMax: -5}}, true},
+		{"example that does not reach target", args{17, -4, TargetArea{xMin: 20, xMax: 30, yMin: -10, yMax: -5}}, false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := willReachTargetArea(tt.args.initialXVelocity, tt.args.initialYVelocity, tt.args.targetArea); got != tt.want {
+				t.Errorf("willReachTargetArea() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_countDistinctInitialVelocitiesThatReachTheTarget(t *testing.T) {
+	type args struct {
+		targetArea TargetArea
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"example input", args{TargetArea{xMin: 20, xMax: 30, yMin: -10, yMax: -5}}, 112},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := countDistinctInitialVelocitiesThatReachTheTarget(tt.args.targetArea); got != tt.want {
+				t.Errorf("countDistinctInitialVelocitiesThatReachTheTarget() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
