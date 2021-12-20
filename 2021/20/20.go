e3ae4d5931648b8ce05df8c14787e5b69373632e
Benjamin Hogl <9270901+EEP-Benny@users.noreply.github.com>
2021-12-20 08:53:43 +0100

    Day 20, part 1, ~50min

---
+++ 2021/20/20.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+
+	"github.com/EEP-Benny/AdventOfCode/utils"
+)
+
+type Image struct {
+	pixels        [][]string
+	outsidePixels string
+}
+
+func main() {
+	input := utils.LoadInput(2021, 20)
+	mapping, inputImage := parseInput(input)
+	imageAfterTwoSteps := enhancementSteps(inputImage, mapping, 2)
+	fmt.Println("Solution 1:", countLitPixels(imageAfterTwoSteps))
+	// fmt.Println("Solution 2:", ???)
+}
+
+func parseInput(input string) (mapping []string, image Image) {
+	splitInput := strings.Split(input, "\n\n")
+	mapping = strings.Split(splitInput[0], "")
+
+	for _, line := range strings.Split(splitInput[1], "\n") {
+		image.pixels = append(image.pixels, strings.Split(line, ""))
+	}
+	image.outsidePixels = "."
+	return mapping, image
+}
+
+func getPixelAt(image Image, y, x int) string {
+	if y < 0 || y >= len(image.pixels) || x < 0 || x >= len(image.pixels[y]) {
+		return image.outsidePixels
+	}
+	return image.pixels[y][x]
+}
+func getSurroundingPixels(image Image, y, x int) [9]string {
+	return [9]string{
+		getPixelAt(image, y-1, x-1),
+		getPixelAt(image, y-1, x),
+		getPixelAt(image, y-1, x+1),
+		getPixelAt(image, y, x-1),
+		getPixelAt(image, y, x),
+		getPixelAt(image, y, x+1),
+		getPixelAt(image, y+1, x-1),
+		getPixelAt(image, y+1, x),
+		getPixelAt(image, y+1, x+1),
+	}
+}
+func pixelsToNumber(pixels [9]string) int {
+	pixelsString := strings.Join(pixels[:], "")
+	binaryString := strings.ReplaceAll(strings.ReplaceAll(pixelsString, "#", "1"), ".", "0")
+	number, _ := strconv.ParseInt(binaryString, 2, 64)
+	return int(number)
+}
+
+func enhancementStep(inputImage Image, mapping []string) (outputImage Image) {
+	ySize := len(inputImage.pixels)
+	xSize := len(inputImage.pixels[0])
+	outputImage.pixels = make([][]string, ySize+2)
+	for y := 0; y < ySize+2; y++ {
+		outputImage.pixels[y] = make([]string, xSize+2)
+		for x := 0; x < xSize+2; x++ {
+			surroundingPixels := getSurroundingPixels(inputImage, y-1, x-1)
+			number := pixelsToNumber(surroundingPixels)
+			outputImage.pixels[y][x] = mapping[number]
+		}
+	}
+	outputImage.outsidePixels = mapping[pixelsToNumber(getSurroundingPixels(inputImage, -10, -10))]
+	return outputImage
+}
+
+func enhancementSteps(image Image, mapping []string, stepCount int) Image {
+	for i := 0; i < stepCount; i++ {
+		image = enhancementStep(image, mapping)
+	}
+	return image
+}
+
+func countLitPixels(image Image) int {
+	count := 0
+	for y := 0; y < len(image.pixels); y++ {
+		for x := 0; x < len(image.pixels[y]); x++ {
+			if image.pixels[y][x] == "#" {
+				count++
+			}
+		}
+	}
+	return count
+}
---
+++ 2021/20/20_test.go
@@ -0,0 +1,181 @@
+package main
+
+import (
+	"reflect"
+	"strings"
+	"testing"
+)
+
+var exampleInput = strings.TrimSpace(`
+..#.#..#####.#.#.#.###.##.....###.##.#..###.####..#####..#....#..#..##..###..######.###...####..#..#####..##..#.#####...##.#.#..#.##..#.#......#.###.######.###.####...#.##.##..#..#..#####.....#.#....###..#.##......#.....#..#..#..##..#...##.######.####.####.#.#...#.......#..#.#.#...####.##.#......#..#...##.#.##..#...##.#.##..###.#......#.#.......#.#.#.####.###.##...#.....####.#..#..#.##.#....##..#.####....##...##..#...#......#.#.......#.......##..####..#...#.#.#...##..#.#..###..#####........#..####......#..#
+
+#..#.
+#....
+##..#
+..#..
+..###
+`)
+var exampleMapping, exampleInputImage = parseInput(exampleInput)
+
+func Test_parseInput(t *testing.T) {
+	type args struct {
+		input string
+	}
+	tests := []struct {
+		name              string
+		args              args
+		wantMappingLength int
+		wantImage         Image
+	}{
+		{"exampleInput", args{exampleInput}, 512, Image{
+			outsidePixels: ".",
+			pixels: [][]string{
+				{"#", ".", ".", "#", "."},
+				{"#", ".", ".", ".", "."},
+				{"#", "#", ".", ".", "#"},
+				{".", ".", "#", ".", "."},
+				{".", ".", "#", "#", "#"},
+			},
+		}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gotMapping, gotImage := parseInput(tt.args.input)
+			if len(gotMapping) != tt.wantMappingLength {
+				t.Errorf("parseInput() gotMapping = %v, want string with length %v", gotMapping, tt.wantMappingLength)
+			}
+			if !reflect.DeepEqual(gotImage, tt.wantImage) {
+				t.Errorf("parseInput() gotImage = %v, want %v", gotImage, tt.wantImage)
+			}
+		})
+	}
+}
+
+func Test_getSurroundingPixels(t *testing.T) {
+	type args struct {
+		image Image
+		y     int
+		x     int
+	}
+	tests := []struct {
+		name string
+		args args
+		want [9]string
+	}{
+		{"center pixel from example", args{exampleInputImage, 2, 2}, [9]string{".", ".", ".", "#", ".", ".", ".", "#", "."}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := getSurroundingPixels(tt.args.image, tt.args.y, tt.args.x); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("getSurroundingPixels() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_pixelsToNumber(t *testing.T) {
+	type args struct {
+		pixels [9]string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"center pixel from example", args{[9]string{".", ".", ".", "#", ".", ".", ".", "#", "."}}, 34},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := pixelsToNumber(tt.args.pixels); got != tt.want {
+				t.Errorf("pixelsToNumber() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_enhancementStep(t *testing.T) {
+	type args struct {
+		inputImage Image
+		mapping    []string
+	}
+	tests := []struct {
+		name string
+		args args
+		want Image
+	}{
+		{"exampleInput first step", args{exampleInputImage, exampleMapping}, Image{
+			outsidePixels: ".",
+			pixels: [][]string{
+				{".", "#", "#", ".", "#", "#", "."},
+				{"#", ".", ".", "#", ".", "#", "."},
+				{"#", "#", ".", "#", ".", ".", "#"},
+				{"#", "#", "#", "#", ".", ".", "#"},
+				{".", "#", ".", ".", "#", "#", "."},
+				{".", ".", "#", "#", ".", ".", "#"},
+				{".", ".", ".", "#", ".", "#", "."},
+			},
+		}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := enhancementStep(tt.args.inputImage, tt.args.mapping); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("enhancementStep() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_enhancementSteps(t *testing.T) {
+	type args struct {
+		image     Image
+		mapping   []string
+		stepCount int
+	}
+	tests := []struct {
+		name string
+		args args
+		want Image
+	}{
+		{"exampleInput two step", args{exampleInputImage, exampleMapping, 2}, Image{
+			outsidePixels: ".",
+			pixels: [][]string{
+				{".", ".", ".", ".", ".", ".", ".", "#", "."},
+				{".", "#", ".", ".", "#", ".", "#", ".", "."},
+				{"#", ".", "#", ".", ".", ".", "#", "#", "#"},
+				{"#", ".", ".", ".", "#", "#", ".", "#", "."},
+				{"#", ".", ".", ".", ".", ".", "#", ".", "#"},
+				{".", "#", ".", "#", "#", "#", "#", "#", "."},
+				{".", ".", "#", ".", "#", "#", "#", "#", "#"},
+				{".", ".", ".", "#", "#", ".", "#", "#", "."},
+				{".", ".", ".", ".", "#", "#", "#", ".", "."},
+			},
+		}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := enhancementSteps(tt.args.image, tt.args.mapping, tt.args.stepCount); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("enhancementSteps() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_countLitPixels(t *testing.T) {
+	type args struct {
+		image Image
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"exampleInput after two step", args{enhancementSteps(exampleInputImage, exampleMapping, 2)}, 35},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := countLitPixels(tt.args.image); got != tt.want {
+				t.Errorf("countLitPixels() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
