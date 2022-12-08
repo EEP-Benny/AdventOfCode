aa91e4d91acb1e2f0d0c87bef9c87f762d2eeb89
Benjamin Hogl <9270901+EEP-Benny@users.noreply.github.com>
2022-12-08 09:25:19 +0100

    Day 8, part 2, ~15min

--- src/year2022/day08.rs
+++ src/year2022/day08.rs
@@ -56,12 +56,68 @@ fn count_visible_trees(grid: &GridOfTrees) -> u32 {
     count
 }
 
+fn get_viewing_distance_in_direction(
+    grid: &GridOfTrees,
+    tree_position: Position,
+    direction: Position,
+) -> u32 {
+    let tree_size = grid[tree_position.1 as usize][tree_position.0 as usize];
+    let mut position = tree_position;
+    let mut count_of_visible_trees = 0;
+    loop {
+        position.0 += direction.0;
+        position.1 += direction.1;
+        match grid
+            .get(position.1 as usize)
+            .and_then(|row| row.get(position.0 as usize))
+        {
+            Some(size) => {
+                count_of_visible_trees += 1;
+                if size >= &tree_size {
+                    return count_of_visible_trees; // no further trees visible
+                };
+            }
+            None => {
+                return count_of_visible_trees; // found the edge
+            }
+        };
+    }
+}
+
+fn get_scenic_score(grid: &GridOfTrees, tree_position: Position) -> u32 {
+    get_viewing_distance_in_direction(grid, tree_position, (0, -1))
+        * get_viewing_distance_in_direction(grid, tree_position, (1, 0))
+        * get_viewing_distance_in_direction(grid, tree_position, (0, 1))
+        * get_viewing_distance_in_direction(grid, tree_position, (-1, 0))
+}
+
+fn get_highest_scenic_score(grid: &GridOfTrees) -> u32 {
+    let mut highest_scenic_score = 0;
+    for y in 0..grid.len() {
+        for x in 0..grid[y].len() {
+            let scenic_score = get_scenic_score(grid, (x as i32, y as i32));
+            if scenic_score > highest_scenic_score {
+                highest_scenic_score = scenic_score;
+            }
+        }
+    }
+    highest_scenic_score
+}
+
 fn part1(input: &str) -> u32 {
     count_visible_trees(&parse_input_string(input))
 }
 
+fn part2(input: &str) -> u32 {
+    get_highest_scenic_score(&parse_input_string(input))
+}
+
 pub fn solution1() -> u32 {
     part1(&get_input(2022, 08))
+}
+
+pub fn solution2() -> u32 {
+    part2(&get_input(2022, 08))
 }
 
 #[cfg(test)]
@@ -148,12 +204,63 @@ mod tests {
     }
 
     #[test]
+    fn test_get_viewing_distance_in_direction() {
+        let grid_of_trees = parse_input_string(EXAMPLE_INPUT.trim());
+
+        // the top middle 5
+        assert_eq!(
+            get_viewing_distance_in_direction(&grid_of_trees, (2, 1), (0, -1)),
+            1
+        );
+        assert_eq!(
+            get_viewing_distance_in_direction(&grid_of_trees, (2, 1), (-1, 0)),
+            1
+        );
+        assert_eq!(
+            get_viewing_distance_in_direction(&grid_of_trees, (2, 1), (1, 0)),
+            2
+        );
+        assert_eq!(
+            get_viewing_distance_in_direction(&grid_of_trees, (2, 1), (0, 1)),
+            2
+        );
+
+        // the bottom middle 5
+        assert_eq!(
+            get_viewing_distance_in_direction(&grid_of_trees, (2, 3), (0, -1)),
+            2
+        );
+        assert_eq!(
+            get_viewing_distance_in_direction(&grid_of_trees, (2, 3), (-1, 0)),
+            2
+        );
+        assert_eq!(
+            get_viewing_distance_in_direction(&grid_of_trees, (2, 3), (0, 1)),
+            1
+        );
+        assert_eq!(
+            get_viewing_distance_in_direction(&grid_of_trees, (2, 3), (1, 0)),
+            2
+        );
+    }
+
+    #[test]
+    fn test_get_scenic_score() {
+        let grid_of_trees = parse_input_string(EXAMPLE_INPUT.trim());
+
+        assert_eq!(get_scenic_score(&grid_of_trees, (2, 1)), 4);
+        assert_eq!(get_scenic_score(&grid_of_trees, (2, 3)), 8);
+    }
+
+    #[test]
     fn test_parts() {
         assert_eq!(part1(EXAMPLE_INPUT.trim()), 21);
+        assert_eq!(part2(EXAMPLE_INPUT.trim()), 8);
     }
 
     #[test]
     fn test_solutions() {
         assert_eq!(solution1(), 1832);
+        assert_eq!(solution2(), 157320);
     }
 }
