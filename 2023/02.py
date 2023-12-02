83effb4c53a64241540dd07b76b23ccc913dfdfd
Benjamin Hogl <9270901+EEP-Benny@users.noreply.github.com>
2023-12-02 12:55:19 +0100

    Day 2, part 1 (~30min)

---
+++ 2023/02.py
@@ -0,0 +1,60 @@
+import re
+
+from utils import getInput
+from dataclasses import dataclass
+
+
+@dataclass
+class SetOfCubes:
+    red: int
+    green: int
+    blue: int
+
+    def __le__(self, other: "SetOfCubes"):
+        return (
+            self.red <= other.red
+            and self.green <= other.green
+            and self.blue <= other.blue
+        )
+
+
+def parseSetOfCubes(string: str) -> SetOfCubes:
+    red_match = re.search(r"(\d+) red", string)
+    green_match = re.search(r"(\d+) green", string)
+    blue_match = re.search(r"(\d+) blue", string)
+    red = int(red_match[1]) if red_match is not None else 0
+    green = int(green_match[1]) if green_match is not None else 0
+    blue = int(blue_match[1]) if blue_match is not None else 0
+    return SetOfCubes(red, green, blue)
+
+
+@dataclass
+class Game:
+    id: int
+    sets_of_cubes: "list[SetOfCubes]"
+
+    def is_possible(self, available_cubes: SetOfCubes):
+        return all(
+            set_of_cubes <= available_cubes for set_of_cubes in self.sets_of_cubes
+        )
+
+
+def parseGame(line: str) -> Game:
+    match = re.match(r"Game (\d+): (.*)", line)
+    assert match is not None
+    id = int(match[1])
+    sets_of_cubes = [parseSetOfCubes(string) for string in match[2].split(";")]
+    return Game(id, sets_of_cubes)
+
+
+input = getInput(2023, 2)
+games = [parseGame(line) for line in input]
+
+
+def solution1():
+    available_cubes = SetOfCubes(red=12, green=13, blue=14)
+    return sum([game.id for game in games if game.is_possible(available_cubes)])
+
+
+if __name__ == "__main__":
+    print(solution1())
