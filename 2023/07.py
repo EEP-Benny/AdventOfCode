8b29e0296f19aab0abf819ea696555eabc4417a6
Benjamin Hogl <9270901+EEP-Benny@users.noreply.github.com>
2023-12-07 12:57:14 +0100

    Day 7, refactoring for part 2 (~10min)

--- 2023/07.py
+++ 2023/07.py
@@ -12,30 +12,35 @@ card_strengths = {
 }
 
 
+def get_score_from_counter(counter: Counter) -> int:
+    counts = [count for _, count in counter.most_common()]
+    if counts[0] == 5:
+        return 5  # Five of a kind
+    elif counts[0] == 4:
+        return 4  # Four of a kind
+    elif counts[0] == 3 and counts[1] == 2:
+        return 3.5  # Full house
+    elif counts[0] == 3:
+        return 3  # Three of a kind
+    elif counts[0] == 2 and counts[1] == 2:
+        return 2  # Two pair
+    elif counts[0] == 2:
+        return 1  # One pair
+    else:
+        return 0  # High card
+
+
 @dataclass
 class Hand:
     cards: "list[str]"
 
     def get_score(self) -> int:
         counter = Counter(self.cards)
-        counts = [count for el, count in counter.most_common()]
-        if counts[0] == 5:
-            return 5  # Five of a kind
-        elif counts[0] == 4:
-            return 4  # Four of a kind
-        elif counts[0] == 3 and counts[1] == 2:
-            return 3.5  # Full house
-        elif counts[0] == 3:
-            return 3  # Three of a kind
-        elif counts[0] == 2 and counts[1] == 2:
-            return 2  # Two pair
-        elif counts[0] == 2:
-            return 1  # One pair
-        else:
-            return 0  # High card
+        return get_score_from_counter(counter)
 
-    def get_decorated(self) -> "tuple[int, list[int], Hand]":
-        return (self.get_score(), [card_strengths[card] for card in self.cards], self)
+
+def decorate(hand: Hand) -> "tuple[int, list[int]]":
+    return (hand.get_score(), [card_strengths[card] for card in hand.cards])
 
 
 def parse_input_line(line: str) -> "tuple[Hand, int]":
@@ -47,21 +52,23 @@ def parse_input(input: "list[str]"):
     return [parse_input_line(line) for line in input]
 
 
+def calculate_winnings(hands_and_bids, get_decoration) -> int:
+    decorated_hands_and_bids = [
+        (get_decoration(hand), hand, bid) for hand, bid in hands_and_bids
+    ]
+    decorated_hands_and_bids.sort(key=itemgetter(0))
+    sorted_hands_and_bids = [(hand, bid) for _, hand, bid in decorated_hands_and_bids]
+    return sum(
+        (index + 1) * bid for index, (_, bid) in enumerate(sorted_hands_and_bids)
+    )
+
+
 input = getInput(2023, 7)
 hands_and_bids = parse_input(input)
 
 
 def solution1():
-    decorated_hands_and_bids = [
-        (hand.get_decorated(), bid) for hand, bid in hands_and_bids
-    ]
-    decorated_hands_and_bids.sort(key=itemgetter(0))
-    sorted_hands_and_bids = [
-        (decorated_hand[-1], bid) for decorated_hand, bid in decorated_hands_and_bids
-    ]
-    return sum(
-        (index + 1) * bid for index, (hand, bid) in enumerate(sorted_hands_and_bids)
-    )
+    return calculate_winnings(hands_and_bids, decorate)
 
 
 def solution2():
