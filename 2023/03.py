36a9dfcabeef7254a49b4f006df58fc7b5813f91
Benjamin Hogl <9270901+EEP-Benny@users.noreply.github.com>
2023-12-10 12:30:38 +0100

    Day 3, part 2 (~20min)

--- 2023/03.py
+++ 2023/03.py
@@ -1,5 +1,7 @@
 import re
-from collections import UserList
+from collections import UserList, defaultdict
+from dataclasses import dataclass
+
 
 from utils import getInput
 
@@ -8,16 +10,42 @@ def is_symbol(string: str) -> bool:
     return re.match(r"[\.\d]", string) is None
 
 
+@dataclass
+class NumberWithSymbol:
+    number: int
+    symbol: str
+    x: int
+    y: int
+
+
 class Schematic(UserList[str]):
-    def is_symbol_around(self, x: int, y: int) -> bool:
+    def get_symbol_around(
+        self, x: int, y: int, number: int
+    ) -> "NumberWithSymbol | None":
         for dx in [-1, 0, 1]:
             for dy in [-1, 0, 1]:
                 try:
                     if is_symbol(self[y + dy][x + dx]):
-                        return True
+                        return NumberWithSymbol(
+                            symbol=self[y + dy][x + dx],
+                            x=x + dx,
+                            y=y + dy,
+                            number=number,
+                        )
                 except IndexError:
                     pass
-        return False
+        return None
+
+    def get_numbers_around_symbols(self) -> list[NumberWithSymbol]:
+        numbers_with_symbols: list[NumberWithSymbol] = []
+        for y, line in enumerate(self):
+            for match in re.finditer(r"\d+", line):
+                number = int(match.group(0))
+                match_at_start = schematic.get_symbol_around(match.start(), y, number)
+                match_at_end = schematic.get_symbol_around(match.end() - 1, y, number)
+                if match_at_start or match_at_end:
+                    numbers_with_symbols.append(match_at_start or match_at_end)
+        return numbers_with_symbols
 
 
 input = getInput(2023, 3)
@@ -25,18 +53,25 @@ schematic = Schematic(input)
 
 
 def solution1():
-    part_numbers = []
-    for y, line in enumerate(schematic):
-        for match in re.finditer(r"\d+", line):
-            has_symbol_at_start = schematic.is_symbol_around(match.start(), y)
-            has_symbol_at_end = schematic.is_symbol_around(match.end() - 1, y)
-            if has_symbol_at_start or has_symbol_at_end:
-                part_numbers.append(int(match.group(0)))
-    return sum(part_numbers)
+    return sum(
+        number_with_symbol.number
+        for number_with_symbol in schematic.get_numbers_around_symbols()
+    )
 
 
 def solution2():
-    return
+    gear_numbers = defaultdict(list)
+    for number_with_symbol in schematic.get_numbers_around_symbols():
+        if number_with_symbol.symbol == "*":
+            gear_numbers[(number_with_symbol.x, number_with_symbol.y)].append(
+                number_with_symbol.number
+            )
+
+    return sum(
+        numbers[0] * numbers[1]
+        for numbers in gear_numbers.values()
+        if len(numbers) == 2
+    )
 
 
 if __name__ == "__main__":
