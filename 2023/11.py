332d12c338ccb94e1ba8942d88a8fb66c7ca5e3c
Benjamin Hogl <9270901+EEP-Benny@users.noreply.github.com>
2023-12-11 09:04:45 +0100

    Day 11, part 2 (~3min)

--- 2023/11.py
+++ 2023/11.py
@@ -12,7 +12,7 @@ class Coordinate:
 class Universe:
     galaxies: list[Coordinate]
 
-    def expand(self) -> "Universe":
+    def expand(self, factor: int) -> "Universe":
         x_coords = set(c.x for c in self.galaxies)
         y_coords = set(c.y for c in self.galaxies)
         x_offset = 0
@@ -23,15 +23,22 @@ class Universe:
             if x in x_coords:
                 x_mapping[x] = x + x_offset
             else:
-                x_offset += 1
+                x_offset += factor - 1
         for y in range(max(y_coords) + 1):
             if y in y_coords:
                 y_mapping[y] = y + y_offset
             else:
-                y_offset += 1
+                y_offset += factor - 1
         return Universe(
             [Coordinate(x_mapping[c.x], y_mapping[c.y]) for c in self.galaxies]
         )
+
+    def get_sum_of_shortest_paths(self) -> int:
+        sum_of_distances = 0
+        for c1 in self.galaxies:
+            for c2 in self.galaxies:
+                sum_of_distances += abs(c1.x - c2.x) + abs(c1.y - c2.y)
+        return sum_of_distances // 2
 
 
 def parse_input(input: "list[str]") -> Universe:
@@ -48,16 +55,13 @@ universe = parse_input(input)
 
 
 def solution1():
-    expanded_universe = universe.expand()
-    sum_of_distances = 0
-    for c1 in expanded_universe.galaxies:
-        for c2 in expanded_universe.galaxies:
-            sum_of_distances += abs(c1.x - c2.x) + abs(c1.y - c2.y)
-    return sum_of_distances // 2
+    expanded_universe = universe.expand(2)
+    return expanded_universe.get_sum_of_shortest_paths()
 
 
 def solution2():
-    return
+    expanded_universe = universe.expand(1000000)
+    return expanded_universe.get_sum_of_shortest_paths()
 
 
 if __name__ == "__main__":
