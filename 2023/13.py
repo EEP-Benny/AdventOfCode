3bd9a88d51059070ce71756ab4d6763018eeb056
Benjamin Hogl <9270901+EEP-Benny@users.noreply.github.com>
2023-12-13 10:07:12 +0100

    Day 13, part 2 (~25min)

--- 2023/13.py
+++ 2023/13.py
@@ -1,4 +1,5 @@
 from collections import UserList
+from typing import Generator
 from utils import getRawInput
 
 
@@ -8,8 +9,10 @@ class Pattern(UserList[str]):
             ["".join(self[x][y] for x in range(len(self))) for y in range(len(self[0]))]
         )
 
-    def find_mirror_line(self) -> "int | None":
+    def find_mirror_line(self, forbidden_line: "int | None") -> "int | None":
         for mirror_line in range(0, len(self) - 1):
+            if mirror_line + 1 == forbidden_line:
+                continue
             for upper, lower in zip(
                 range(mirror_line, -1, -1), range(mirror_line + 1, len(self))
             ):
@@ -18,6 +21,38 @@ class Pattern(UserList[str]):
             else:
                 # no break in inner loop means that everything is symmetrical
                 return mirror_line + 1  # counting starts at 1
+
+    def get_reflection_summary(
+        self, forbidden_summary: "int | None" = None
+    ) -> "int | None":
+        vertical_line = self.find_mirror_line(
+            forbidden_summary / 100 if forbidden_summary is not None else None
+        )
+        if vertical_line is not None:
+            return vertical_line * 100
+        else:
+            return self.get_flipped().find_mirror_line(
+                forbidden_summary if forbidden_summary is not None else None
+            )
+
+    def get_unsmudged(self, smudge_x: int, smudge_y: int) -> "Pattern":
+        def unsmudge_line(line, smudge_pos):
+            if line[smudge_pos] == "#":
+                return line[:smudge_pos] + "." + line[smudge_pos + 1 :]
+            else:
+                return line[:smudge_pos] + "#" + line[smudge_pos + 1 :]
+
+        return Pattern(
+            [
+                line if y != smudge_y else unsmudge_line(line, smudge_x)
+                for y, line in enumerate(self)
+            ]
+        )
+
+    def get_all_unsmudged(self) -> "Generator[Pattern]":
+        for x in range(len(self[0])):
+            for y in range(len(self)):
+                yield self.get_unsmudged(x, y)
 
 
 def parse_input(input: str) -> list[Pattern]:
@@ -29,19 +64,21 @@ patterns = parse_input(input)
 
 
 def solution1():
-    summary = 0
-    for pattern in patterns:
-        vertical_line = pattern.find_mirror_line()
-        if vertical_line is not None:
-            summary += vertical_line * 100
-        else:
-            horizontal_line = pattern.get_flipped().find_mirror_line()
-            summary += horizontal_line
-    return summary
+    return sum(pattern.get_reflection_summary() for pattern in patterns)
 
 
 def solution2():
-    return
+    summary = 0
+    for pattern in patterns:
+        smudged_mirror_summary = pattern.get_reflection_summary()
+        for unsmudged_pattern in pattern.get_all_unsmudged():
+            mirror_summary = unsmudged_pattern.get_reflection_summary(
+                forbidden_summary=smudged_mirror_summary
+            )
+            if mirror_summary is not None:
+                summary += mirror_summary
+                break
+    return summary
 
 
 if __name__ == "__main__":
