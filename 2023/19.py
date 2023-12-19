e407ab8820c6f02aab30c90e77573b10d78492f3
Benjamin Hogl <9270901+EEP-Benny@users.noreply.github.com>
2023-12-19 12:44:28 +0100

    Day 19, part 2 (~35min)

--- 2023/19.py
+++ 2023/19.py
@@ -1,5 +1,5 @@
 import re
-from dataclasses import dataclass
+from dataclasses import dataclass, replace
 from collections import UserDict
 from utils import getRawInput
 
@@ -7,6 +7,45 @@ from utils import getRawInput
 class Part(UserDict[str, int]):
     def get_rating_sum(self):
         return self["x"] + self["m"] + self["a"] + self["s"]
+
+
+def split_range(r: range, value: int) -> tuple[range, range]:
+    return (range(r.start, value), range(value, r.end))
+
+
+@dataclass
+class CombinationRange:
+    x: range
+    m: range
+    a: range
+    s: range
+
+    def split_at(
+        self, prop: str, value: int
+    ) -> tuple["CombinationRange", "CombinationRange"]:
+        if prop == "x":
+            return (
+                replace(self, x=range(self.x.start, value)),
+                replace(self, x=range(value, self.x.stop)),
+            )
+        if prop == "m":
+            return (
+                replace(self, m=range(self.m.start, value)),
+                replace(self, m=range(value, self.m.stop)),
+            )
+        if prop == "a":
+            return (
+                replace(self, a=range(self.a.start, value)),
+                replace(self, a=range(value, self.a.stop)),
+            )
+        if prop == "s":
+            return (
+                replace(self, s=range(self.s.start, value)),
+                replace(self, s=range(value, self.s.stop)),
+            )
+
+    def get_size(self) -> int:
+        return len(self.x) * len(self.m) * len(self.a) * len(self.s)
 
 
 @dataclass
@@ -33,6 +72,27 @@ class Workflow:
                     return rule.next_workflow
         return self.fallback_workflow
 
+    def get_next_workflows_for_combinations(
+        self, combinations: CombinationRange
+    ) -> list[tuple[str, CombinationRange]]:
+        next_workflows: list[tuple[str, CombinationRange]] = []
+        remaining_combinations = combinations
+        for rule in self.rules:
+            if rule.comp_is_lt:
+                left, right = remaining_combinations.split_at(
+                    rule.comp_prop, rule.comp_value
+                )
+                next_workflows.append((rule.next_workflow, left))
+                remaining_combinations = right
+            else:
+                left, right = remaining_combinations.split_at(
+                    rule.comp_prop, rule.comp_value + 1
+                )
+                next_workflows.append((rule.next_workflow, right))
+                remaining_combinations = left
+        next_workflows.append((self.fallback_workflow, remaining_combinations))
+        return next_workflows
+
 
 @dataclass
 class System:
@@ -43,6 +103,29 @@ class System:
         while workflow not in ["R", "A"]:
             workflow = self.workflows[workflow].get_next_workflow(part)
         return workflow == "A"
+
+    def count_accepted_combinations(self) -> int:
+        all_combinations = CombinationRange(
+            x=range(1, 4001),
+            m=range(1, 4001),
+            a=range(1, 4001),
+            s=range(1, 4001),
+        )
+        accepted_combinations: list[CombinationRange] = []
+        workflows_to_consider = [("in", all_combinations)]
+        while workflows_to_consider:
+            workflow_name, combinations = workflows_to_consider.pop()
+            if workflow_name == "R":
+                continue
+            if workflow_name == "A":
+                accepted_combinations.append(combinations)
+                continue
+            workflows_to_consider.extend(
+                self.workflows[workflow_name].get_next_workflows_for_combinations(
+                    combinations
+                )
+            )
+        return sum(comb.get_size() for comb in accepted_combinations)
 
 
 def parse_part(input: str) -> Part:
@@ -92,7 +175,7 @@ def solution1():
 
 
 def solution2():
-    return
+    return system.count_accepted_combinations()
 
 
 if __name__ == "__main__":
