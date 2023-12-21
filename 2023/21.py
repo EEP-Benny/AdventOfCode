a9307a115726c75fc8fc78b3590bdf881aebe3c2
Benjamin Hogl <9270901+EEP-Benny@users.noreply.github.com>
2023-12-21 15:05:39 +0100

    Day 21, part 2 (~75min)

--- 2023/21.py
+++ 2023/21.py
@@ -23,6 +23,7 @@ class Position:
 class Garden:
     garden_plots: set[Position]
     starting_position: Position
+    size: int
 
 
 def parse_input(input: list[str]) -> Garden:
@@ -36,24 +37,28 @@ def parse_input(input: list[str]) -> Garden:
             if char == "S":
                 starting_position = Position(x, y)
             garden_plots.add(Position(x, y))
+    size = len(input)
 
-    return Garden(garden_plots, starting_position)
+    return Garden(garden_plots, starting_position, size)
 
 
 input = getInput(2023, 21)
 garden = parse_input(input)
 
 
-def solution1():
+def step_and_count(steps: int) -> int:
     positions_to_explore = [garden.starting_position]
     reachable_positions = set[Position](positions_to_explore)
-    max_steps = 64
-    for step in range(max_steps):
+    for _ in range(steps):
         positions_to_explore_next = []
         for pos in positions_to_explore:
             for neighbor in pos.get_neighbors():
+                wrapped_neighbor = Position(
+                    neighbor.x % garden.size, neighbor.y % garden.size
+                )
+
                 if (
-                    neighbor in garden.garden_plots
+                    wrapped_neighbor in garden.garden_plots
                     and neighbor not in reachable_positions
                 ):
                     reachable_positions.add(neighbor)
@@ -62,12 +67,23 @@ def solution1():
     return sum(
         1
         for pos in reachable_positions
-        if pos.is_even() == garden.starting_position.is_even()
+        if (pos.is_even() == garden.starting_position.is_even()) == (steps % 2 == 0)
     )
 
 
+def solution1():
+    return step_and_count(64)
+
+
 def solution2():
-    return
+    steps_to_try = [i * garden.size + garden.starting_position.x for i in range(3)]
+    results = [f"{{{steps}, {step_and_count(steps)}}}" for steps in steps_to_try]
+
+    return (
+        "https://www.wolframalpha.com/input?i=InterpolatingPolynomial[{"
+        + ",".join(results)
+        + "}, x]/.{x=26501365}"
+    )
 
 
 if __name__ == "__main__":
