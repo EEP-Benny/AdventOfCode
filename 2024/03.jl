657ec1a162c07b32e6a98f26a168bdc146d1724a
Benjamin Hogl <9270901+EEP-Benny@users.noreply.github.com>
2024-12-03 09:23:33 +0100

    Day 3, part 2 (~10min)

--- 2024/03.jl
+++ 2024/03.jl
@@ -4,14 +4,27 @@ using .AdventOfCodeUtils
 
 function part1(input)
     sum_of_multiplications = 0
-    for m in eachmatch(r"mul\((\d{1,3}),(\d{1,3})\)", input)
-        sum_of_multiplications += parse(Int, m.captures[1]) * parse(Int, m.captures[2])
+    for m in eachmatch(r"mul\((?<num1>\d{1,3}),(?<num2>\d{1,3})\)", input)
+        sum_of_multiplications += parse(Int, m["num1"]) * parse(Int, m["num2"])
     end
     sum_of_multiplications
 end
 
 function part2(input)
-    nothing
+    is_multiplication_enabled = true
+    sum_of_multiplications = 0
+    for m in eachmatch(r"do\(\)|don't\(\)|mul\((?<num1>\d{1,3}),(?<num2>\d{1,3})\)", input)
+        if startswith(m.match, "don't(")
+            is_multiplication_enabled = false
+        elseif startswith(m.match, "do(")
+            is_multiplication_enabled = true
+        elseif startswith(m.match, "mul(")
+            if is_multiplication_enabled
+                sum_of_multiplications += parse(Int, m["num1"]) * parse(Int, m["num2"])
+            end
+        end
+    end
+    sum_of_multiplications
 end
 
 if abspath(PROGRAM_FILE) == @__FILE__
--- 2024/03.test.jl
+++ 2024/03.test.jl
@@ -1,16 +1,14 @@
 include("03.jl")
 using Test
 
-example_input = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
 
 @testset "Example Input" begin
-    input = example_input
-    @test part1(input) === 161
-    @test part2(input) === nothing
+    @test part1("xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))") === 161
+    @test part2("xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))") === 48
 end
 
 @testset "Real input" begin
     input = get_input(day=3, year=2024)
     @test part1(input) === 187825547
-    @test part2(input) === nothing
+    @test part2(input) === 85508223
 end
