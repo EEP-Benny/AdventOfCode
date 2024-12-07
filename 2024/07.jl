9715d30cde94bdc9882087981e51b3bf124f46b6
Benjamin Hogl <9270901+EEP-Benny@users.noreply.github.com>
2024-12-07 13:27:46 +0100

    Day 7, part 1 (~25min)

---
+++ 2024/07.jl
@@ -0,0 +1,50 @@
+include("utils.jl")
+
+using .AdventOfCodeUtils
+import Base.:(==)
+
+struct CalibrationEquation
+    test_value::Int64
+    numbers::Vector{Int64}
+end
+
+function Base.parse(::Type{CalibrationEquation}, input)::CalibrationEquation
+    parts = split(input, ": ")
+    test_value = parse(Int64, parts[1])
+    numbers = parse.(Int64, split(parts[2], " "))
+    CalibrationEquation(test_value, numbers)
+end
+
+function ==(a::CalibrationEquation, b::CalibrationEquation)
+    a.test_value == b.test_value && a.numbers == b.numbers
+end
+
+function prepare_input(input::AbstractString)::Vector{CalibrationEquation}
+    parse.(CalibrationEquation, split(input, "\n"))
+end
+
+function could_possibly_true(equation::CalibrationEquation)::Bool
+    function inner(first, second=nothing, rest...)
+        if second === nothing
+            return first === equation.test_value
+        end
+        return inner(first + second, rest...) || inner(first * second, rest...)
+    end
+    inner(equation.numbers...)
+end
+
+function part1(input)
+    sum(map(eq -> eq.test_value, filter(could_possibly_true, input)))
+end
+
+function part2(input)
+    nothing
+end
+
+if abspath(PROGRAM_FILE) == @__FILE__
+    input = prepare_input(get_input(day=7, year=2024))
+    @show part1(input)
+    @show part2(input)
+    @showtime part1(input)
+    @showtime part2(input)
+end
---
+++ 2024/07.test.jl
@@ -0,0 +1,38 @@
+include("07.jl")
+using Test
+
+example_input = """
+190: 10 19
+3267: 81 40 27
+83: 17 5
+156: 15 6
+7290: 6 8 6 15
+161011: 16 10 13
+192: 17 8 14
+21037: 9 7 18 13
+292: 11 6 16 20"""
+
+@testset "parse Equation" begin
+    @test parse(CalibrationEquation, "190: 10 19") == CalibrationEquation(190, [10, 19])
+    @test parse(CalibrationEquation, "3267: 81 40 27") == CalibrationEquation(3267, [81, 40, 27])
+    @test parse(CalibrationEquation, "161011: 16 10 13") == CalibrationEquation(161011, [16, 10, 13])
+end
+
+@testset "could_possibly_true" begin
+    @test could_possibly_true(parse(CalibrationEquation, "190: 10 19")) == true
+    @test could_possibly_true(parse(CalibrationEquation, "3267: 81 40 27")) == true
+    @test could_possibly_true(parse(CalibrationEquation, "292: 11 6 16 20")) == true
+    @test could_possibly_true(parse(CalibrationEquation, "161011: 16 10 13")) == false
+end
+
+@testset "Example Input" begin
+    input = prepare_input(example_input)
+    @test part1(input) === 3749
+    @test part2(input) === nothing
+end
+
+@testset "Real input" begin
+    input = prepare_input(get_input(day=7, year=2024))
+    @test part1(input) === 5702958180383
+    @test part2(input) === nothing
+end
