21962519cf3e03a8fa5749b6a1b3dc0642808316
Benjamin Hogl <9270901+EEP-Benny@users.noreply.github.com>
2024-12-12 10:32:24 +0100

    Day 12, part 1 (~45min)

---
+++ 2024/12.jl
@@ -0,0 +1,73 @@
+include("utils.jl")
+
+using .AdventOfCodeUtils
+
+function prepare_input(input::AbstractString)
+    collect.(split(input, "\n"))
+end
+
+mutable struct Region
+    label::Char
+    area::Int
+    perimeter::Int
+end
+
+# import Base.==
+
+# function ==(a::Region, b::Region)
+#     a.label == b.label && a.area == b.area && a.perimeter == b.perimeter
+# end
+
+function get_regions(input::Vector{Vector{Char}})::Set{Region}
+    position_to_region = Dict{Tuple{Int,Int},Region}()
+    regions = Set{Region}()
+    for (y, line) in enumerate(input), (x, label) in enumerate(line)
+        position = (x, y)
+        if haskey(position_to_region, position)
+            continue
+        end
+        function has_same_label((x, y)::Tuple{Int,Int})::Bool
+            checkbounds(Bool, input, y) && checkbounds(Bool, input[y], x) && input[y][x] == label
+        end
+        region = Region(label, 0, 0)
+        position_to_region[position] = region
+        positions_to_explore = Set([position])
+        while !isempty(positions_to_explore)
+            new_positions_to_explore = Set{Tuple{Int,Int}}()
+            for position in positions_to_explore
+                position_to_region[position] = region
+                region.area += 1
+                for direction in [(0, 1), (1, 0), (-1, 0), (0, -1)]
+                    next_position = position .+ direction
+                    if has_same_label(next_position)
+                        if !haskey(position_to_region, next_position)
+                            push!(new_positions_to_explore, next_position)
+                        end
+                    else
+                        region.perimeter += 1
+                    end
+                end
+            end
+            positions_to_explore = new_positions_to_explore
+        end
+        push!(regions, region)
+    end
+    regions
+end
+
+
+function part1(input)
+    sum(region.area * region.perimeter for region in get_regions(input))
+end
+
+function part2(input)
+    nothing
+end
+
+if abspath(PROGRAM_FILE) == @__FILE__
+    input = prepare_input(get_input(day=12, year=2024))
+    @show part1(input)
+    @show part2(input)
+    @showtime part1(input)
+    @showtime part2(input)
+end
---
+++ 2024/12.test.jl
@@ -0,0 +1,51 @@
+include("12.jl")
+using Test
+
+example_input_small = """
+AAAA
+BBCD
+BBCC
+EEEC"""
+
+example_input = """
+RRRRIICCFF
+RRRRIICCCF
+VVRRRCCFFF
+VVRCCCJFFF
+VVVVCJJCFE
+VVIVCCJJEE
+VVIIICJJEE
+MIIIIIJJEE
+MIIISIJEEE
+MMMISSJEEE"""
+
+@testset "parse input" begin
+    @test prepare_input(example_input_small) == [
+        ['A', 'A', 'A', 'A'],
+        ['B', 'B', 'C', 'D'],
+        ['B', 'B', 'C', 'C'],
+        ['E', 'E', 'E', 'C'],]
+end
+
+# @testset "get_regions" begin
+#     @test get_regions(prepare_input(example_input_small)) == Set([
+#         Region('A', 4, 10),
+#         Region('B', 4, 8),
+#         Region('C', 4, 10),
+#         Region('D', 1, 4),
+#         Region('E', 3, 8)
+#     ])
+# end
+
+
+@testset "Example Input" begin
+    input = prepare_input(example_input)
+    @test part1(input) === 1930
+    @test part2(input) === nothing
+end
+
+@testset "Real input" begin
+    input = prepare_input(get_input(day=12, year=2024))
+    @test part1(input) === 1488414
+    @test part2(input) === nothing
+end
