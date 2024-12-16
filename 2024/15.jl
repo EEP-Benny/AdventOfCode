2713a6e7998c84436fe6510cafc161ab45edae42
Benjamin Hogl <9270901+EEP-Benny@users.noreply.github.com>
2024-12-16 16:33:38 +0100

    Day 15, part 2 (~90min)

--- 2024/15.jl
+++ 2024/15.jl
@@ -43,6 +43,53 @@ function do_the_moves((robot, boxes, walls, movements))
     end
     robot, boxes
 end
+
+function collect_wide_boxes(push_point::Tuple{Int,Int}, push_direction::Tuple{Int,Int}, boxes::Set{Tuple{Int,Int}}, walls::Set{Tuple{Int,Int}})
+    if push_point .+ push_direction ∈ walls
+        error("hit a wall at $(push_point .+ push_direction)")
+    end
+    if push_point .+ push_direction ∈ boxes && push_direction != (-1, 0)
+        return [
+            push_point .+ push_direction,
+            collect_wide_boxes(push_point .+ push_direction, push_direction, boxes, walls)...,
+            collect_wide_boxes(push_point .+ push_direction .+ (1, 0), push_direction, boxes, walls)...
+        ]
+    end
+    if push_point .+ push_direction .- (1, 0) ∈ boxes && push_direction != (1, 0)
+        return [
+            push_point .+ push_direction .- (1, 0),
+            collect_wide_boxes(push_point .+ push_direction .- (1, 0), push_direction, boxes, walls)...,
+            collect_wide_boxes(push_point .+ push_direction, push_direction, boxes, walls)...
+        ]
+    end
+    return []
+end
+
+function do_the_moves_but_wide((robot, boxes, walls, movements))
+    inflate_pos((x, y)) = (2x - 1, y)
+
+    robot = inflate_pos(robot)
+    boxes = Set(inflate_pos.(boxes))
+    walls = Set(inflate_pos.(walls))
+    union!(walls, Set((x + 1, y) for (x, y) in walls))
+
+    for movement in movements
+        try
+            boxes_to_move = collect_wide_boxes(robot, movement, boxes, walls)
+            for box in boxes_to_move
+                delete!(boxes, box)
+            end
+            for box in boxes_to_move
+                push!(boxes, box .+ movement)
+            end
+            robot = robot .+ movement
+        catch
+            # do nothing if we hit a wall
+        end
+    end
+    robot, boxes
+end
+
 function get_gps_coordinates(boxes)
     sum((box[1] - 1) + 100 * (box[2] - 1) for box in boxes)
 end
@@ -54,7 +101,8 @@ end
 
 
 function part2(input)
-    nothing
+    _, boxes = do_the_moves_but_wide(input)
+    get_gps_coordinates(boxes)
 end
 
 if abspath(PROGRAM_FILE) == @__FILE__
--- 2024/15.test.jl
+++ 2024/15.test.jl
@@ -37,15 +37,27 @@ vvv<<^>^v^^><<>>><>^<<><^vv^^<>vvv<>><^^v>^>vv<>v<<<<v<^v>^<^^>>>^<v<v
 v^^>>><<^^<>>^v^<v^vv<>v^<<>^<^v^v><^<<<><<^<v><v<>vv>>v><v^<vv<>v^<<^"""
 
 
+example_input_small_part_2 = """
+#######
+#...#.#
+#.....#
+#..OO@#
+#..O..#
+#.....#
+#######
+
+<vv<<^^<<^^"""
+
 @testset "Example Input" begin
     input = prepare_input(example_input)
     @test part1(prepare_input(example_input_small)) === 2028
     @test part1(input) === 10092
-    @test part2(input) === nothing
+    @test part2(prepare_input(example_input_small_part_2)) === 618
+    @test part2(input) === 9021
 end
 
 @testset "Real input" begin
     input = prepare_input(get_input(day=15, year=2024))
     @test part1(input) === 1515788
-    @test part2(input) === nothing
+    @test part2(input) === 1516544
 end
