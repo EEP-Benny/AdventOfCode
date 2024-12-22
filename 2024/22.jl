a0682ca8eb1316943ff0532a3f651acd2fc1c56d
Benjamin Hogl <9270901+EEP-Benny@users.noreply.github.com>
2024-12-22 14:05:27 +0100

    Day 22, part 2 (~45min coding, ~20min computing)

--- 2024/22.jl
+++ 2024/22.jl
@@ -12,21 +12,67 @@ function evolve_secret_number(secret_number::Int)
     secret_number = ((secret_number * 2048) ⊻ secret_number) % 16777216
 end
 
-function evolve_2000(secret_number::Int)
-    for _ in 1:2000
+function get_2000_secrets(secret_number::Int)
+    secrets = Vector{Int}(undef, 2001)
+    for i in 1:2001
+        secrets[i] = secret_number
         secret_number = evolve_secret_number(secret_number)
     end
-    secret_number
+    secrets
 end
 
+function get_bananas(secret_number::Int)
+    secret_number % 10
+end
 
 function part1(input)
-    sum(evolve_2000, input)
+    sum(secret_number -> get_2000_secrets(secret_number)[end], input)
+end
+
+function get_bananas_for_diff_sequence(bananas::Vector{Int}, diff_sequence::Vector{Int})
+    differences = diff(bananas)
+    for banana_index in eachindex(differences)
+        is_valid_sequence = true
+        for sequence_index in eachindex(diff_sequence)
+            test_index = banana_index + sequence_index - 1
+            if test_index > length(differences) || differences[test_index] != diff_sequence[sequence_index]
+                is_valid_sequence = false
+                break
+            end
+        end
+        if is_valid_sequence
+            return bananas[banana_index+length(diff_sequence)]
+        end
+    end
+    return 0
+end
+
+function get_all_sequences()
+    sequences = []
+    for a in -9:9
+        for b in -9:9
+            for c in -9:9
+                for d in -9:9
+                    push!(sequences, [a, b, c, d])
+                end
+            end
+        end
+    end
+    sequences
 end
 
 
 function part2(input)
-    nothing
+    bananas = [get_bananas.(get_2000_secrets(initial_secret)) for initial_secret in input]
+    max_bananas = 0
+    for (i, sequence) in enumerate(get_all_sequences())
+        if (all(x -> x == 0, sequence[3:end]))
+            @show i, sequence
+        end
+        bananas_for_this_sequence = sum(get_bananas_for_diff_sequence(b, sequence) for b in bananas)
+        max_bananas = max(max_bananas, bananas_for_this_sequence)
+    end
+    max_bananas
 end
 
 if abspath(PROGRAM_FILE) == @__FILE__
--- 2024/22.test.jl
+++ 2024/22.test.jl
@@ -1,14 +1,20 @@
 include("22.jl")
 using Test
 
-example_input = """
+example_input_1 = """
 1
 10
 100
 2024"""
 
+example_input_2 = """
+1
+2
+3
+2024"""
+
 @testset "prepare_input" begin
-    @test prepare_input(example_input) == [1, 10, 100, 2024]
+    @test prepare_input(example_input_1) == [1, 10, 100, 2024]
 end
 
 @testset "evolve_secret_number" begin
@@ -22,21 +28,34 @@ end
     @test evolve_secret_number(704524) == 1553684
 end
 
-@testset "evolve_2000" begin
-    @test evolve_2000(1) == 8685429
-    @test evolve_2000(10) == 4700978
-    @test evolve_2000(100) == 15273692
-    @test evolve_2000(2024) == 8667524
+@testset "get_2000_secrets" begin
+    @test get_2000_secrets(1)[end] == 8685429
+    @test get_2000_secrets(10)[end] == 4700978
+    @test get_2000_secrets(100)[end] == 15273692
+    @test get_2000_secrets(2024)[end] == 8667524
+end
+
+@testset "banana maths" begin
+    secrets = get_2000_secrets(123)[1:10]
+    @test get_bananas.(secrets) == [3, 0, 6, 5, 4, 4, 6, 4, 4, 2]
+    @test diff(get_bananas.(secrets)) == [-3, 6, -1, -1, 0, 2, -2, 0, -2]
+end
+
+@testset "get_bananas_for_diff_sequence" begin
+    @test get_bananas_for_diff_sequence(get_bananas.(get_2000_secrets(123)[1:10]), [-1, -1, 0, 2]) == 6
+    @test get_bananas_for_diff_sequence(get_bananas.(get_2000_secrets(1)), [-2, 1, -1, 3]) == 7
+    @test get_bananas_for_diff_sequence(get_bananas.(get_2000_secrets(2)), [-2, 1, -1, 3]) == 7
+    @test get_bananas_for_diff_sequence(get_bananas.(get_2000_secrets(3)), [-2, 1, -1, 3]) == 0
+    @test get_bananas_for_diff_sequence(get_bananas.(get_2000_secrets(2024)), [-2, 1, -1, 3]) == 9
 end
 
 @testset "Example Input" begin
-    input = prepare_input(example_input)
-    @test part1(input) === 37327623
-    @test part2(input) === nothing
+    @test part1(prepare_input(example_input_1)) === 37327623
+    @test part2(prepare_input(example_input_2)) === 23
 end
 
 @testset "Real input" begin
     input = prepare_input(get_input(day=22, year=2024))
     @test part1(input) === 13185239446
-    @test part2(input) === nothing
+    @test part2(input) === 1501
 end
