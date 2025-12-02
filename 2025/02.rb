3f782c6303f1597fad9bcd705a95ea17ef279969
Benjamin Hogl <9270901+EEP-Benny@users.noreply.github.com>
2025-12-02 08:29:06 +0100

    Day 2, part 2 (~25min)

--- 2025/02.rb
+++ 2025/02.rb
@@ -18,21 +18,39 @@ module Day02
   end
 
   def get_invalid_ids(range)
-    invalid_ids = []
-    range.each do |id|
+    range.filter do |id|
       id_string = id.to_s
       next if id_string.length.odd?
 
       split_point = id_string.length.div 2
       left = id_string[...split_point]
       right = id_string[split_point..]
-      invalid_ids.append(id) if left == right
+
+      left == right
     end
-    invalid_ids
+  end
+
+  def get_advanced_invalid_ids(range)
+    range.filter do |id|
+      id_string = id.to_s
+      length = id_string.length
+      possible_split_lengths = (1..length / 2).select { |n| (length % n).zero? }
+      # puts id_string
+      # puts possible_split_lengths
+      possible_split_lengths.any? do |split_length|
+        chunks = (length / split_length).times.map { |i| id_string[i * split_length, split_length] }
+        # puts split_length, ':', chunks
+        chunks.uniq.length == 1
+      end
+    end
   end
 
   def part1(input)
     input.flat_map { |range| get_invalid_ids(range) }.sum
+  end
+
+  def part2(input)
+    input.flat_map { |range| get_advanced_invalid_ids(range) }.sum
   end
 
   Utils.run_benchmark_for(self) if __FILE__ == $PROGRAM_NAME
--- 2025/test/02_test.rb
+++ 2025/test/02_test.rb
@@ -31,13 +31,29 @@ class TestDay02 < Minitest::Test
     assert_equal([38_593_859], get_invalid_ids(38_593_856..38_593_862))
   end
 
+  def test_get_advanced_invalid_ids
+    assert_equal([11, 22], get_advanced_invalid_ids(11..22))
+    assert_equal([99, 111], get_advanced_invalid_ids(95..115))
+    assert_equal([999, 1010], get_advanced_invalid_ids(998..1012))
+    assert_equal([1_188_511_885], get_advanced_invalid_ids(1_188_511_880..1_188_511_890))
+    assert_equal([222_222], get_advanced_invalid_ids(222_220..222_224))
+    assert_equal([], get_advanced_invalid_ids(1_698_522..1_698_528))
+    assert_equal([446_446], get_advanced_invalid_ids(446_443..446_449))
+    assert_equal([38_593_859], get_advanced_invalid_ids(38_593_856..38_593_862))
+    assert_equal([565_656], get_advanced_invalid_ids(565_653..565_659))
+    assert_equal([824_824_824], get_advanced_invalid_ids(824_824_821..824_824_827))
+    assert_equal([2_121_212_121], get_advanced_invalid_ids(2_121_212_118..2_121_212_124))
+  end
+
   def test_example_input
     input = prepare_input(@example_input)
     assert_equal(1_227_775_554, part1(input))
+    assert_equal(4_174_379_265, part2(input))
   end
 
   def test_real_input
     input = prepare_input(real_input)
     assert_equal(16_793_817_782, part1(input))
+    assert_equal(27_469_417_404, part2(input))
   end
 end
