d6e5e385add0fef84ec1b2511916b1f640343d87
Benjamin Hogl <9270901+EEP-Benny@users.noreply.github.com>
2025-12-03 06:38:06 +0100

    Day 3, part 2 (~7min)

--- 2025/03.rb
+++ 2025/03.rb
@@ -16,14 +16,25 @@ module Day03
     end
   end
 
-  def get_bank_joltage(bank)
-    ten_digit = bank[...-1].max
-    one_digit = bank[bank.index(ten_digit) + 1..].max
-    ten_digit * 10 + one_digit
+  def get_bank_joltage(bank, num_cells)
+    final_joltage = 0
+    start_index = 0
+    num_cells.times.each do |cell_index|
+      final_joltage *= 10
+      last_index = bank.length - num_cells + cell_index
+      max_digit = bank[start_index..last_index].max
+      start_index = bank[start_index..].index(max_digit) + start_index + 1
+      final_joltage += max_digit
+    end
+    final_joltage
   end
 
   def part1(input)
-    input.map { |bank| get_bank_joltage(bank) }.sum
+    input.map { |bank| get_bank_joltage(bank, 2) }.sum
+  end
+
+  def part2(input)
+    input.map { |bank| get_bank_joltage(bank, 12) }.sum
   end
 
   Utils.run_benchmark_for(self) if __FILE__ == $PROGRAM_NAME
--- 2025/test/03_test.rb
+++ 2025/test/03_test.rb
@@ -29,19 +29,26 @@ class TestDay03 < Minitest::Test
   end
 
   def test_get_bank_joltage
-    assert_equal(98, get_bank_joltage([9, 8, 7, 6, 5, 4, 3, 2, 1, 1, 1, 1, 1, 1, 1]))
-    assert_equal(89, get_bank_joltage([8, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 9]))
-    assert_equal(78, get_bank_joltage([2, 3, 4, 2, 3, 4, 2, 3, 4, 2, 3, 4, 2, 7, 8]))
-    assert_equal(92, get_bank_joltage([8, 1, 8, 1, 8, 1, 9, 1, 1, 1, 1, 2, 1, 1, 1]))
+    assert_equal(98, get_bank_joltage([9, 8, 7, 6, 5, 4, 3, 2, 1, 1, 1, 1, 1, 1, 1], 2))
+    assert_equal(89, get_bank_joltage([8, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 9], 2))
+    assert_equal(78, get_bank_joltage([2, 3, 4, 2, 3, 4, 2, 3, 4, 2, 3, 4, 2, 7, 8], 2))
+    assert_equal(92, get_bank_joltage([8, 1, 8, 1, 8, 1, 9, 1, 1, 1, 1, 2, 1, 1, 1], 2))
+
+    assert_equal(987_654_321_111, get_bank_joltage([9, 8, 7, 6, 5, 4, 3, 2, 1, 1, 1, 1, 1, 1, 1], 12))
+    assert_equal(811_111_111_119, get_bank_joltage([8, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 9], 12))
+    assert_equal(434_234_234_278, get_bank_joltage([2, 3, 4, 2, 3, 4, 2, 3, 4, 2, 3, 4, 2, 7, 8], 12))
+    assert_equal(888_911_112_111, get_bank_joltage([8, 1, 8, 1, 8, 1, 9, 1, 1, 1, 1, 2, 1, 1, 1], 12))
   end
 
   def test_example_input
     input = prepare_input(@example_input)
     assert_equal(357, part1(input))
+    assert_equal(3_121_910_778_619, part2(input))
   end
 
   def test_real_input
     input = prepare_input(real_input)
     assert_equal(17_142, part1(input))
+    assert_equal(169_935_154_100_102, part2(input))
   end
 end
