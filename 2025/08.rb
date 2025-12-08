273d74b8cdfb0570c5375eab4026923968092402
Benjamin Hogl <9270901+EEP-Benny@users.noreply.github.com>
2025-12-08 22:14:25 +0100

    Day 8, performance optimizations

--- 2025/08.rb
+++ 2025/08.rb
@@ -10,21 +10,25 @@ module Day08
     Utils.get_input(8, 2025)
   end
 
-  Position = Struct.new(:x, :y, :z) do
-    def distance(other)
-      Math.sqrt((x - other.x)**2 + (y - other.y)**2 + (z - other.z)**2)
-    end
-  end
+  Position = Struct.new(:x, :y, :z)
 
   Graph = Struct.new(:positions, :connections) do
+    def connect_circuits(circuits, pos1, pos2)
+      circuit_pos1 = circuits.find_index { |circuit| circuit.include? pos1 }
+      circuit_pos2 = circuits.find_index { |circuit| circuit.include? pos2 }
+      return circuits if circuit_pos1 == circuit_pos2
+
+      circuit1 = circuits[circuit_pos1]
+      circuit2 = circuits[circuit_pos2]
+      circuits[circuit_pos1] = circuit1 | circuit2
+      circuits[circuit_pos2] = nil
+      circuits.compact!
+    end
+
     def circuit_sizes
       circuits = positions.map { |position| Set[position] }
       connections.each do |pos1, pos2|
-        circuit1 = circuits.find { |circuit| circuit.include? pos1 }
-        circuit2 = circuits.find { |circuit| circuit.include? pos2 }
-        circuits.delete(circuit1)
-        circuits.delete(circuit2)
-        circuits.push(circuit1 | circuit2)
+        circuits = connect_circuits(circuits, pos1, pos2)
       end
       circuits.map(&:size).sort.reverse
     end
@@ -32,11 +36,7 @@ module Day08
     def last_necessary_connection
       circuits = positions.map { |position| Set[position] }
       connections.each do |pos1, pos2|
-        circuit1 = circuits.find { |circuit| circuit.include? pos1 }
-        circuit2 = circuits.find { |circuit| circuit.include? pos2 }
-        circuits.delete(circuit1)
-        circuits.delete(circuit2)
-        circuits.push(circuit1 | circuit2)
+        circuits = connect_circuits(circuits, pos1, pos2)
         return [pos1, pos2] if circuits.size == 1
       end
     end
@@ -50,7 +50,7 @@ module Day08
   end
 
   def get_shortest_connections(positions)
-    positions.combination(2).sort_by { |pos1, pos2| pos1.distance(pos2) }
+    positions.combination(2).sort_by { |pos1, pos2| (pos1.x - pos2.x)**2 + (pos1.y - pos2.y)**2 + (pos1.z - pos2.z)**2 }
   end
 
   def part1(input)
